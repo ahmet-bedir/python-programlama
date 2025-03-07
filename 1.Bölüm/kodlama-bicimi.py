85b0dd56a253d9cc4eb8258af9990c6a96fb8af7
termux <ahmet.bedir767@gmail.com>
2025-03-07 23:20:42 +0300

    kodlama-bicimi.py olusturuldu

---
+++ 1.Bölüm/kodlama-bicimi.py
@@ -0,0 +1,12 @@
+"""
+# -*- coding: ASCII -*-
+python 2x seri varsayılan kodlama biçimi
+
+# -*- coding: cp1254 -*-
+
+# -*- coding: utf-8 -*-
+python 3x seri varsayılan kodlama biçimi
+"""
+# -*- coding: cp1254 -*-
+print("Günaydın...")
+
