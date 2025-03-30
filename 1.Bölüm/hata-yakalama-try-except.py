5b14c48fce504b5ad6cef84ad301337f7f1a817f
termux <ahmet.bedir767@gmail.com>
2025-03-30 12:44:50 +0300

    hata-yakalama.py güncellendi...

--- 1.Bölüm/hata-yakalama-try-except.py
+++ 1.Bölüm/hata-yakalama-try-except.py
@@ -33,6 +33,9 @@ while True:
 Her hata için ayrı bir mesaj göstermek en iyisidir. Ama tabii dilerseniz hata türlerini gruplayıp hepsi için tek bir hata mesajı göstermeyi de tercih edebilirsiniz.
 '''
 ###
+'''
+Bütün kodlarımızı tek bir try... bloğu içine koymaktansa istersek bu kodlarda verilebilecek hataları gruplamayı da tercih edebiliriz.
+'''
 try:
     bolunen = int(input("bölünecek sayı: "))
     bolen = int(input("bölen sayı: "))
@@ -43,15 +46,16 @@ else:
         print(bolunen/bolen)
     except ZeroDivisionError:
         print("Bir sayıyı 0'a bölemezsiniz!")
+
+###
 '''
 try:
     ...bir takım işler...
-except birHata:
+except HataAdı:
     ...hata alınınca yapılacak işlemler...
 finally:
     ...hata olsa da olmasa da yapılması gerekenler...
 '''
-###
 try:
     dosya = open("dosyaadı", "r")
     ...burada dosyayla bazı işlemler yapıyoruz...
@@ -60,10 +64,11 @@ except IOError:
     print("Bir Hata Oluştu!")
 finally:
     dosya.close()
+
+###
 '''
 Kullanıcının yaptığı bir işlem normal şartlar altında hata vermeyecek olsa bile biz ona ‘Python tarzı’ bir hata mesajı göstermek için raise deyimini kullanıyoruz.
 '''
-###
 bolunen = int(input("bölünecek sayı: "))
 
 if bolunen == 23:
@@ -85,10 +90,10 @@ for i in parola:
 
 print("Parola kabul edildi!")
 
+###
 '''
 raise deyimini, bir hata mesajına ek olarak bir işlem yapmak istediğimizde de kullanabiliriz.
 '''
-###
 try:
     bolunen = 7
     bolen = 0
@@ -97,19 +102,38 @@ except ZeroDivisionError:
     print("bir sayıyı 0'a bölemezsiniz!")
     raise
 
-####
 
-####
-giris = " "
+#### 
+giris = input("Adınız : ")
+if len(giris) == 0:
+    raise AssertionError("İsim bölümü boş!")
+print("Hoşgeldiniz.")
+
+#### assert ifadesi ile...
+giris = input("Adınız : ")
 assert len(giris) != 0 , "İsim bölümü boş."
 print("Hoşgeldiniz.")
 
-# bütün hatalar için
+### Bütün Hataları Yakalamak İçin...
 try:
-    birtakım kodlar
-except ValueError:
-    print("Yanlış değer")
-except ZeroDivisionError:
-    print("Sıfıra bölme hatası")
+    ...birtakım kodlar...
 except:
     print("Beklenmeyen bir hata oluştu!")
+
+### Python’ın 2.7 öncesi sürümlerinde sys modülünün version_info() metodu farklı çıktılar verir. Mesela Python’ın 2.7 öncesi sürümlerinde version_info() metodunun major, minor veya micro gibi nitelikleri bulunmaz. Bu nitelikler Python programlama diline 2.7 sürümüyle birlikte geldi. Dolayısıyla yukarıdaki programı Python’ın 2.7 öncesi sürümlerinden biriyle çalıştıran kullanıcılarınız istediğiniz çıktıyı alamayacakdır.
+import sys
+
+_2x_metni = """
+Python'ın 2.x sürümlerinden birini kullanıyorsunuz.
+Programı çalıştırabilmek için sisteminizde Python'ın
+3.x sürümlerinden biri kurulu olmalı."""
+
+_3x_metni = "Programa hoşgeldiniz."
+
+try:
+    if sys.version_info.major < 3:
+        print(_2x_metni)
+    else:
+        print(_3x_metni)
+except AttributeError:
+    print(_2x_metni)
