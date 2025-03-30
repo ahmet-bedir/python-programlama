3a4f028b1895ce0c797e29e75b706e36e4b79cb4
termux <ahmet.bedir767@gmail.com>
2025-03-30 10:17:21 +0300

    hata-yakalama.py güncellendi

--- 1.Bölüm/hata-yakalama-try-except.py
+++ 1.Bölüm/hata-yakalama-try-except.py
@@ -1,44 +1,68 @@
-"""
-
-#except (ValueError, ZeroDivisionError):
-
-#except ValueError as hata:
-    #print("Hata Açıklaması : ", hata)
-#except ValueError as hata:
-    #print("Sadece sayı girin!")
-    #print("Orijinal hata mesajı: ", hata)
-    
-#except (ValueError, ZeroDivisionError) as hata:
-    #print("Bir hata oluştu!")
-    #print("Orijinal hata mesajı: ", hata)
-
-###
+'''
+try:
+    ...hata verebileceğini bildiğimiz kodlar...
+except HataAdı:
+    ...hata durumunda yapılacak işlem...
+'''
 while True:
-    ilk_sayi = input("ilk sayı (Programdan çıkmak için q tuşuna basın): ")
-
-    if ilk_sayi == "q":
+    sayi1 = input("1.sayı (çıkış : q) : ")
+    if sayi1 == 'q':
         break
-
-    ikinci_sayi = input("ikinci sayı: ")
-
+    sayi2 = input("2.sayı : ")
     try:
-        sayi1 = int(ilk_sayi)
-        sayi2 = int(ikinci_sayi)
-        print(sayi1, "/", sayi2, "=", sayi1 / sayi2)
-    except (ValueError, ZeroDivisionError):
-        print("Bir hata oluştu!")
-        print("Lütfen tekrar deneyin!")
-
+        sayi1 = int(sayi1)
+        sayi2 = int(sayi2)
+        print("Bölme İşleminin Sonucu :", sayi1/sayi2)
+    #except ValueError:
+        #print("Lütfen Sayı Giriniz!")
+    #except ZeroDivisionError:
+        #print("Sıfıra Bölünme Hatası!")
+    #except (ValueError, ZeroDivisionError): #hata türlerini grupladık.
+    	#print("Bir Hata Oluştu!")
+    	
+    #except ValueError as hata:
+    	#print("Lütfen Sayı Giriniz!")
+    	#print("Hata açıklaması :", hata)
+    #except ZeroDivisionError as hata:
+    	#print("Sıfıra Bölünme Hatası!")
+    	#print("Hata açıklaması :", hata)
+    except (ValueError, ZeroDivisionError) as hata:
+        print("Bir Hata Oluştu!")
+        print("Hata açıklaması :", hata)
+'''
+Her hata için ayrı bir mesaj göstermek en iyisidir. Ama tabii dilerseniz hata türlerini gruplayıp hepsi için tek bir hata mesajı göstermeyi de tercih edebilirsiniz.
+'''
+###
+try:
+    bolunen = int(input("bölünecek sayı: "))
+    bolen = int(input("bölen sayı: "))
+except ValueError:
+    print("Lütfen sadece sayı girin!")
+else:
+    try:
+        print(bolunen/bolen)
+    except ZeroDivisionError:
+        print("Bir sayıyı 0'a bölemezsiniz!")
+'''
+try:
+    ...bir takım işler...
+except birHata:
+    ...hata alınınca yapılacak işlemler...
+finally:
+    ...hata olsa da olmasa da yapılması gerekenler...
+'''
 ###
 try:
     dosya = open("dosyaadı", "r")
     ...burada dosyayla bazı işlemler yapıyoruz...
     ...ve ansızın bir hata oluşuyor...
 except IOError:
-    print("bir hata oluştu!")
+    print("Bir Hata Oluştu!")
 finally:
     dosya.close()
-
+'''
+Kullanıcının yaptığı bir işlem normal şartlar altında hata vermeyecek olsa bile biz ona ‘Python tarzı’ bir hata mesajı göstermek için raise deyimini kullanıyoruz.
+'''
 ###
 bolunen = int(input("bölünecek sayı: "))
 
@@ -61,21 +85,20 @@ for i in parola:
 
 print("Parola kabul edildi!")
 
+'''
+raise deyimini, bir hata mesajına ek olarak bir işlem yapmak istediğimizde de kullanabiliriz.
+'''
 ###
 try:
-    bölünen = 6
-    bölen = 6
-    print(bölünen/bölen)
+    bolunen = 7
+    bolen = 0
+    print(bolunen/bolen)
 except ZeroDivisionError:
-    print("bir sayıyı 0'a bölemezsiniz")
+    print("bir sayıyı 0'a bölemezsiniz!")
     raise
 
 ####
-giris = ""
-if len(giris) == 0:
-    raise AssertionError("İsim bölümü boş.")
-print("Hoşgeldiniz.")
-"""
+
 ####
 giris = " "
 assert len(giris) != 0 , "İsim bölümü boş."
