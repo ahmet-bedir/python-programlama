4c6eaf1eebd9b1f52629e87d32f8f26f6baa4182
ahmetl <ahmet.bedir767@gmail.com>
2025-05-02 23:31:51 +0300

    bicimlendirme.py güncellendi

--- 4.Karakter Dizileri/karakter-dizilerini-bicimlendirme.py
+++ 4.Karakter Dizileri/karakter-dizilerini-bicimlendirme.py
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
-isim = input("isminiz : ")
-if len(isim) <= 5:
-    print(isim[:5])
-else:
-    print(isim[:5], "...")
-=======
-### telefon
->>>>>>> 46e40f6a1470e871727d047710d0a2ccbf7a4814
+### % İşareti ile Biçimlendirme (Eski Yöntem)
+
+parola = input("parola: ")
+
+print("Girdiğiniz parola (%s) kurallara uygun bir paroladır!" %parola)
+
+#
+print("%s ve %s iyi bir ikilidir!" %("Python","Django"))
