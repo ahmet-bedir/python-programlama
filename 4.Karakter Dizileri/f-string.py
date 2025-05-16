3a05032c810859418812d0600ac0eeab376163ea
termux <ahmet.bedir767@gmail.com>
2025-05-16 12:49:59 +0300

    güncelleme işlemi yapıldı...

--- 4.Karakter Dizileri/f-string.py
+++ 4.Karakter Dizileri/f-string.py
@@ -1,1 +1,25 @@
 ###
+isim = 'Buğra'
+print(f'Selam {isim}!')
+
+##
+isim = 'Ahmet'
+yas = 36
+metin = f'Benim adım {isim} ve ben {yas} yaşındayım.'
+print(metin)
+
+## aynı işlem format() metoduyla...
+isim = 'Ahmet'
+yas = 36
+metin = 'Benim adım {} ve ben {} yaşındayım.'
+print(metin.format(isim,yas))
+
+#
+sayi1 = 3
+sayi2 = 2
+print(F'{sayi1} + {sayi2} = {sayi1+sayi2}')
+#
+sayi1 = 3
+sayi2 = 2
+print('{} ++ {} = {}'.format(sayi1,sayi2,sayi1+sayi2))
+
