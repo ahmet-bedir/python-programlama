eeaea209ee1c98237b0c29882888ce4267e32fd6
termux <ahmet.bedir767@gmail.com>
2025-05-16 10:21:57 +0300

    güncelleme işlemi yapıldı...

--- 4.Karakter Dizileri/f-string.py
+++ 4.Karakter Dizileri/f-string.py
@@ -7,3 +7,9 @@ isim = 'Ahmet'
 yas = 36
 metin = f'Benim adım {isim} ve ben {yas} yaşındayım.'
 print(metin)
+
+## aynı işlem format() metoduyla...
+isim = 'Ahmet'
+yas = 36
+metin = 'Benim adım {} ve ben {} yaşındayım.'
+print(metin.format(isim,yas))
