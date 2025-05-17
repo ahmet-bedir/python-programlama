b3cef0f5cc497be54b7c3ba9a8e444ca73979bfd
ahmet bedir <ahmet.bedir767@gmail.com>
2025-05-17 12:31:01 +0300

    güncelleme işlemi yapıldı.

--- 5.Listeler/liste.py
+++ 5.Listeler/liste.py
@@ -3,7 +3,11 @@ liste = ["Python", 1, 2.5, True]
 print(f"Liste : {liste}, Türü : {type(liste)}")
 for eleman in liste:
     print(f"Liste Elemanı : '{eleman}' Türü : {type(eleman)}")
+print("Eleman Sayısı :", len(liste))
 
-# güncelleme
-liste[-1] = "Php"
+# listeyi güncelleme.
+liste[1] = "Php"
+print(liste)
+# listeye eleman ekleme.
+liste += [False, 'A', "Ali", 2]
 print(liste)
