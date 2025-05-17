5222f83a06cb07b80b03d2d26c69b2ecf42edf75
ahmet bedir <ahmet.bedir767@gmail.com>
2025-05-17 12:55:21 +0300

    güncelleme işlemi yapıldı.

--- 5.Listeler/liste.py
+++ 5.Listeler/liste.py
@@ -1,13 +1,26 @@
 ###
 liste = ["Python", 1, 2.5, True]
 print(f"Liste : {liste}, Türü : {type(liste)}")
-for eleman in liste:
-    print(f"Liste Elemanı : '{eleman}' Türü : {type(eleman)}")
+for i, eleman in enumerate(liste,1):
+    print(f"Listenin {i}. elemanı : '{eleman}' türü : {type(eleman)}")
 print("Eleman Sayısı :", len(liste))
 
 # listeyi güncelleme.
 liste[1] = "Php"
 print(liste)
+
 # listeye eleman ekleme.
 liste += [False, 'A', "Ali", 2]
 print(liste)
+
+# listeden eleman silme.
+del liste[0]
+
+# listede eleman arama.
+eleman = "Python"
+varMi = eleman in liste
+if varMi:
+    print(f"Liste içerisinde '{eleman}' elemanı bulunmaktadır.")
+else:
+    print(f"Liste içerisinde '{eleman}' elemanı bulunmamaktadır!")
+
