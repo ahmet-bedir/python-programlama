5f7b91fa46f008efc441c69fab5ebd0015290c45
ahmet bedir <ahmet.bedir767@gmail.com>
2025-05-18 13:51:51 +0300

    value-reference-types.py dosyasında değişiklik yapıldı.

--- 2.Operatörler/uygulama-atama-operatorleri.py
+++ 2.Operatörler/uygulama-atama-operatorleri.py
@@ -1,7 +1,7 @@
-a, *b, c = (1, 2, 3, 4, 'A', 5)
+a, *b, c = (1, 2, 3, 4, 'A', False, 5)
 
 print(a) #1
-print(b) #[2, 3, 4, 'A']
+print(b) #[2, 3, 4, 'A', False]
 print(c) #5
 
 toplam = b[0] + b[1] + b[2]
