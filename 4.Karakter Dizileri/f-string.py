ef5e8865d15305b84a2b688172e2a3d4177a9ef8
termux <ahmet.bedir767@gmail.com>
2025-05-19 11:42:51 +0300

    güncelleme işlemi yapıldı...

--- 4.Karakter Dizileri/f-string.py
+++ 4.Karakter Dizileri/f-string.py
@@ -19,3 +19,20 @@ sayi1 = 3
 sayi2 = 2
 print(F'{sayi1} + {sayi2} = {sayi1+sayi2}')
 
+#
+print(f'Sayıların toplamı {int(input("Birinci sayıyı girin: ")) + int(input("İkinci sayıyı girin: "))} eder.')
+
+
+fstring = "f-string"
+f"{{ {fstring}: f'{{ifade}}' şeklinde kullanılır. }}"
+"{ f-string: f'{ifade}' şeklinde kullanılır. }"
+
+kaynak = "Python İstihza"
+yıl = "2022"
+f"{kaynak=} {yıl=}"
+kaynak='Python İstihza' yıl='2022'
+
+istihza = "Python Istihza"
+print(f"|{istihza:^30}|")
+
+print(istihza.center(30))
