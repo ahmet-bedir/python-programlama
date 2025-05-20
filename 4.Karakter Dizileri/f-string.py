661a51d6672608e32fd8162acf6588cbbe145939
termux <ahmet.bedir767@gmail.com>
2025-05-20 10:15:47 +0300

    güncelleme işlemi yapıldı...

--- 4.Karakter Dizileri/f-string.py
+++ 4.Karakter Dizileri/f-string.py
@@ -35,3 +35,43 @@ print(f"|{kaynak:<020}|")
 #
 print(f'Sayıların toplamı {int(input("Birinci sayıyı girin: ")) + int(input("İkinci sayıyı girin: "))} eder.')
 
+# sayı formatlama...
+sayi = 123
+print(f"{sayi:>6}") #'   123'
+
+print(f"{sayi:0>+6}") #'00+123'
+
+print(f"{sayi:0=+6}") #'+00123'
+
+print(f"Binary: {sayi:b} | Octal: {sayi:o} | Hexadecimal: {sayi:x}") #'Binary: 1111011 | Octal: 173 | Hexadecimal: 7b'
+
+print(f"Binary: {sayi:#b} | Octal: {sayi:#o} | Hexadecimal: {sayi:#x}") #'Binary: 0b1111011 | Octal: 0o173 | Hexadecimal: 0x7b'
+
+ondalik = 0.123
+print(f"{ondalik:.2f}") # f | F ondalık formatlama  '0.12'
+
+print(f"{ondalik:.5f}") #'0.12300'
+
+print(f"{ondalik:.5g}") # g | G fazla sıfırlar dahil edilmez  '0.123'
+
+sayi = 123456
+print(f"{sayi:_}") #'123_456'
+
+print(f"{sayi:-^15_}") #'----123_456----'
+
+print(f"{sayi:,}") #'123,456'
+
+islem = 1 / 12
+print(f"{islem:.2%}") # Sonucun 100 ile çarpılmış halini yüzde olarak çıktı verir  '8.33%'
+
+#
+selam = "Hello, World!"
+hizalama = {"Sol": "<", "Orta": "^", "Sağ": ">"}
+genislik = 25
+for hiza, operator in hizalama.items():
+    print(f"{hiza:>5}: '{selam:{operator}{genislik}}'")
+"""
+ Sol: 'Hello, World!            '
+Orta: '      Hello, World!      '
+ Sağ: '            Hello, World!'
+"""
