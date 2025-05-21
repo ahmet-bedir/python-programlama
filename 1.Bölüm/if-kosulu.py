ffa7638c48d3e3b8ad9e2f8d44bd4c841d4c0505
AhmetBedir <ahmet.bedir767@gmail.com>
2025-05-21 13:28:35 +0300

    if-kosulu.py dosyası güncellendi.

--- 1.Bölüm/if-kosulu.py
+++ 1.Bölüm/if-kosulu.py
@@ -1,28 +1,54 @@
-#verimsiz yöntem, çünkü 10'un altında bir değer girilmesi durumunda hem birinci hem ikinci if bloğu çalışır.
-print("ilk yöntem.")
-sayi = int(input("Sayı Giriniz : "))
+"""
+if <koşul>: #koşul True olduğunda kod bloğu çalışır, False olduğunda kod bloğu çalışmaz.
+    <kod bloğu>
+"""
+# if True:
+#     print("kod bloğu...")
+#
+# if (False):
+#     print("kod bloğu...")
 
-if sayi < 10:
-    print("sayı 10'dan küçüktür.")
-  
-if sayi < 20:
-    print("sayı 20'den küçüktür.")
+#
+kosul = (3 == 5)
+if kosul:
+    print("koşul sağlandığında çalışacak kod bloğu...")
 
-if sayi > 20:
-    print("sayı 20'den büyüktür.")
-  
-if sayi > 30:
-    print("sayı 30'dan büyüktür.")
-    
-#if bize olası bütün sonuçları listeler, elif ise sadece doğru olan ilk sonucu verir.
-print("ikinci yöntem.")
-sayi = int(input("Sayı Giriniz : "))
+login = False
+if login:
+    print("giriş başarılı...")
 
-if sayi < 10:
-    print("sayı 10'dan küçüktür.")
-elif sayi < 20:
-    print("sayı 20'den küçüktür.")
-elif sayi > 30:
-    print("sayı 30'dan büyüktür.")
-elif sayi > 20:
-    print("sayı 20'den büyüktür.")
+email = "info@gmail.com"
+parola = "123"
+login = (email == "inf@gmail.com") and (parola == "123")
+if login:
+    print("giriş başarılı...")
+
+### verimsiz yöntem, çünkü 10'un altında bir değer girilmesi durumunda hem birinci hem ikinci if bloğu çalışır.
+# print("ilk yöntem.")
+# sayi = int(input("Sayı Giriniz : "))
+#
+# if sayi < 10:
+#     print("sayı 10'dan küçüktür.")
+#
+# if sayi < 20:
+#     print("sayı 20'den küçüktür.")
+#
+# if sayi > 20:
+#     print("sayı 20'den büyüktür.")
+#
+# if sayi > 30:
+#     print("sayı 30'dan büyüktür.")
+#
+### if bize olası bütün sonuçları listeler, elif ise sadece doğru olan ilk sonucu verir.
+# print("ikinci yöntem.")
+# sayi = int(input("Sayı Giriniz : "))
+#
+# if sayi < 10:
+#     print("sayı 10'dan küçüktür.")
+# elif sayi < 20:
+#     print("sayı 20'den küçüktür.")
+# elif sayi > 30:
+#     print("sayı 30'dan büyüktür.")
+# elif sayi > 20:
+#     print("sayı 20'den büyüktür.")
+#
