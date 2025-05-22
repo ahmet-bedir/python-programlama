0a1b9fb582c0f9997458b9e38e0b12375fc29e67
AhmetBedir <ahmet.bedir767@gmail.com>
2025-05-22 12:39:18 +0300

    enumerate.py dosyası güncellendi.

--- 1.Bölüm/enumerate.py
+++ 1.Bölüm/enumerate.py
@@ -9,8 +9,18 @@ markalar = ["opel","bmw","togg"]
 # for i,marka in enumerate(markalar,1):
 #     print(f"{i}-{marka}")
 
-obj = enumerate(markalar,1)
-print(type(obj))
-obj = list(obj)
-for i,marka in obj:
-    print(f"{i}_{marka}")
+# obj = enumerate(markalar,1)
+# print(type(obj))
+# obj = list(obj)
+# for i,marka in obj:
+#     print(f"{i}_{marka}")
+
+### zip metodu liste birleştirmek için kullanılır.
+
+numara = [100,200,300]
+ogrenci = ["Ali","Ayşe","Canan","Mehmet"]
+
+print(list(zip(numara,ogrenci)))
+
+for no,isim in zip(numara,ogrenci):
+    print(no,isim)
