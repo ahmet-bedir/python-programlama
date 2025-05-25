51216b1780ce8bd699829422b192720954deeb35
AhmetBedir <ahmet.bedir767@gmail.com>
2025-05-25 16:56:34 +0300

    liste.py dosyası değiştirildi.

--- 5.Listeler/liste.py
+++ 5.Listeler/liste.py
@@ -1,38 +1,42 @@
 ### listelerde elemanlar değiştirilebilinir ve yeni eleman eklenebilir.
-liste = ["Python", 1, 2.5, True]
+liste = ["Python", 1, 2.5, [True,False]]
 print(f"Liste : {liste}, Türü : {type(liste)}")
 
 print(liste[0])
 print(liste[1])
 print(liste[2])
-print(liste[3])
+print(liste[3][0])
+print(liste[3][1])
 #print(liste[4]) #hata! liste 4 elemanlı bir liste, biz olmayan elemanı yazdıramayız.
 
 for i, eleman in enumerate(liste,1):
     print(f"Listenin {i}. elemanı : '{eleman}' türü : {type(eleman)}")
+
 print("Eleman Sayısı :", len(liste))
+print("Kullanabileceğimiz metodlar :\n", *dir(list))
 
 # listeyi güncelleme.
-liste[1] = "Php"
-print("Listenin 2.elemanı 'Php' ile değiştirildi.")
-print(liste)
-
+# liste[1] = "Php"
+# print("Listenin 2.elemanı 'Php' ile değiştirildi.")
+# print(liste)
+#
 # listeye eleman ekleme.
-liste += [False, 'A', "Ali", 2]
-print("Listeye 4 eleman daha eklendi.")
-print(liste)
-
+# liste += [False, 'A', "Ali", 2]
+# print("Listeye 4 eleman daha eklendi.")
+# print(liste)
+#
 # listeden eleman silme.
-del liste[0]
-print("Listenin ilk elemanı silindi.")
-print(liste)
-
+# del liste[0]
+# print("Listenin ilk elemanı silindi.")
+# print(liste)
+#
 # listede eleman arama.
-eleman = "Python"
-varMi = eleman in liste
-if varMi:
-    print(f"Liste içerisinde '{eleman}' elemanı bulunmaktadır.")
-else:
-    print(f"Liste içerisinde '{eleman}' elemanı bulunmamaktadır!")
-
-
+# eleman = "Python"
+# varMi = eleman in liste
+# if varMi:
+#     print(f"Liste içerisinde '{eleman}' elemanı bulunmaktadır.")
+# else:
+#     print(f"Liste içerisinde '{eleman}' elemanı bulunmamaktadır!")
+#
+#
+#
