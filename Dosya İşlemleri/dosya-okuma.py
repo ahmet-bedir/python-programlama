864941d9baaf1df0c12bc1923b6fff04ef9f8b59
AhmetBedir <ahmet.bedir767@gmail.com>
2025-05-27 19:53:37 +0300

    dosya-okuma.py eklendi.

---
+++ Dosya İşlemleri/dosya-okuma.py
@@ -0,0 +1,8 @@
+"""
+    Dosya açmak ve oluşturmak için open() fonksiyonu kullanılır.
+    
+    Kullanımı           : open(dosya_adi,dosya_erişme_modu)
+    dosya_erişme_modu   : dosyayı hangi amaçla açtığımızı belirtir.
+    "r" okuma modu      : okuma modu. belirtilen konumda dosya olmalıdır.
+    seek                : cursor konumu
+"""
