9fbed6e1877d833b2d6d935862b6d8282919daef
AhmetBedir <ahmet.bedir767@gmail.com>
2025-05-28 16:07:38 +0300

    dosya-okuma.py güncellendi.

--- Dosya İşlemleri/dosya-okuma.py
+++ Dosya İşlemleri/dosya-okuma.py
@@ -6,3 +6,23 @@
     "r" okuma modu      : okuma modu. belirtilen konumda dosya olmalıdır.
     seek                : cursor konumu
 """
+
+f = open("log.txt",encoding="utf-8")
+
+print("dosyanın tamamını okunur :\n", f.read(), sep='')
+
+f.seek(0) #cursör en başa gelir.
+
+
+print("dosyanın ilk satrı okunur :\n", f.readline(), sep='')
+
+f.seek(0)
+print("dosyanın satırları liste şeklinde okunur :\n", f.readlines(), sep='')
+
+f.seek(0)
+for satir in f.readlines():
+    print(satir, end='')
+
+f.close()
+
+print(f.closed) #dosyanın kapatılma durumunu sorgular.
