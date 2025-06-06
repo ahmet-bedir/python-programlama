e63c99017c60eb65dd78711bc72dc468f3302302
Termux <ahmet.bedir767@gmail.com>
2025-06-06 19:13:52 +0300

    a.py dosyasına python kodları eklendi

--- a.py
+++ a.py
@@ -1,1 +1,8 @@
-input()
+kursAdi = "Python ile Programlama"
+kursAciklama = "Güzel bir kurs"
+kursSuresi = "20 Saat"
+
+mesaj = "kurs adı: " + kursAdi + " " + "kurs acıklaması: " + kursAciklama + " " + "kurs süresi: " + kursSuresi
+
+print(mesaj[-1])
+print(mesaj[0])
