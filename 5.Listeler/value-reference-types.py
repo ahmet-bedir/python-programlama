431703f98df5bcb02e4848f6e985dd7add1c901d
Termux <ahmet.bedir767@gmail.com>
2025-06-10 09:30:02 +0300

    güncelleme işlemi yapıldı...

--- 5.Listeler/value-reference-types.py
+++ 5.Listeler/value-reference-types.py
@@ -1,23 +1,25 @@
 # değer tipler (value types) : bellekte değer olarak tutulur.
-x = 10
+x  = 10
 y = 20
 x = y
 y = 30
-print("x :", x)
-print("y :", y)
+print("x :", x) #x : 20
+print("y :", y) #y : 30
+
 
 # referans tipler (reference types) : bellekte adres olarak tutulur.
 liste1 = [1,2,3]
 liste2 = [4,5,6]
 liste1 = liste2
 liste1[0] = 11
-print("liste1 :", liste1)
-print("liste2 :", liste2)
+print("liste1 :", liste1) #liste1 : [11, 5, 6]
+print("liste2 :", liste2) #liste2 : [11, 5, 6]
 
-# liste kopyalama (değer tip)
+
+# copy metodu ile liste kopyalama (değer tip)
 listeA = [1,2,3]
-listeB = listeA.copy() # 1.yol
-listeB = list(listeA)
+#listeB = listeA.copy() # 1.yol
+listeB = list(listeA) # 2.yol
 listeA[0] = 11
-print("listeA :", listeA)
-print("listeB :", listeB)
+print("listeA :", listeA) #listeA : [11, 2, 3]
+print("listeB :", listeB) #listeB : [1, 2, 3]
