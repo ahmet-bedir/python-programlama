006ba26f4e6b7fc40ad1da4a2205ede40ac0a30e
Termux <ahmet.bedir767@gmail.com>
2025-06-30 10:37:04 +0300

    güncelleme işlemi yapıldı...

---
+++ 6.Sayılar/aritmetik-fonksiyonlar.py
@@ -0,0 +1,13 @@
+###Aritmetik Fonksiyonlar
+#abs() fonksiyonu bize bir sayının mutlak değerini verir:
+
+print(abs(-2)) #2
+
+print(abs(2)) #2
+
+# divmod() fonksiyonu, bir sayının bir sayıya bölünmesi işleminde bölümü ve kalanı verir:
+
+print(divmod(10, 2)) #(5, 0)
+
+print(divmod(14, 3)) #(4, 2)
+
