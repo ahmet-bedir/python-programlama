45dbcb2678d2af5c4312c47f2a9fabb092032b25
Termux <ahmet.bedir767@gmail.com>
2025-07-02 17:17:31 +0300

    dizin ismi güncellendi.

--- uygulamalar/giris-sorgulama.py
+++
@@ -1,7 +0,0 @@
-s1 = input("1.sayı: ")
-s2 = input("2.sayı: ")
-
-if s1 > s2:
-    print("{} x {} = {}".format(s1,s2,int(s1)*int(s2)))
-else:
-    print("{} + {} = {}".format(s1,s2,int(s1)+int(s2)))
