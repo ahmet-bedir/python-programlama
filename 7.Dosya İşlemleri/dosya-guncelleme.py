b55033b78884dd7aaa97b44981fe8c5fc97453db
AhmetBedir <ahmet.bedir767@gmail.com>
2025-07-06 14:00:29 +0300

    güncelleme işlemi yapıldı.

--- 7.Dosya İşlemleri/dosya-guncelleme.py
+++ 7.Dosya İşlemleri/dosya-guncelleme.py
@@ -1,13 +1,13 @@
-# "a": (Append) ekleme. Dosya konumda yoksa oluşturur. Varsa dosyaya ekleme yapar.
-# "r": (Read) okuma. Dosya konumda yoksa hata verir.
-# "w": (Write) yazma modu. 
+# "a" : (Append) Ekleme. Dosya konumda yoksa oluşturur. Varsa dosyaya ekleme yapar.
+# "r" : (Read) Okuma. Dosya konumda yoksa hata verir.
+# "w" : (Write) Yazma modu. 
 #    ** Dosyayı konumda oluşturur. 
 #    ** Dosya mevcut ise dosyayı siler ve yeniden oluşturur. 
 # "r+": Hem okuma hem yazma modunda açılır. Dosya konumda yoksa hata verir.
 
 #Dosyaların Sonunda Değişiklik Yapmak
 with open("markalar.txt","a") as file:
-    file.write("6.Bmw\n")
+    file.write("6-Bmw\n")
 
 
 #Dosyaların Başında Değişiklik Yapmak
@@ -15,26 +15,27 @@ with open("markalar.txt","r+", encoding="utf-8") as file:
      markalar = file.read()
      print(markalar)
      
-     input("Dosyaya ekleme yapmak için herhangi bir tuşa basınız.")
+     input("Dosyaya ekleme yapmak için bir tuşa basınız.")
      
-     markalar = "1.Toyota\n" + markalar
+     markalar = "1-Toyota\n" + markalar
      file.seek(0)
      file.write(markalar)
      
      print(markalar)
 
 
-
-
-
-
-with open("markalar.txt","r+",encoding="utf-8") as file: #dosyanın ortasına ekleme yapar.
+#Dosyaların Ortasında Değişiklik Yapmak
+with open("markalar.txt","r+",encoding="utf-8") as file:
     markalar = file.readlines()
     markalar.insert(2, "3-Renault\n")
     file.seek(0)
+    file.writelines(markalar) #writelines() metodu bir dosyaya yalnızca liste veri tipinde veriler yazabilir.
+    
+    """
+    veya...
     for marka in markalar:
-        file.write(marka)
-    # file.writelines(markalar)
+        file.write(marka) #write() metodu bir dosyaya yalnızca karakter dizilerini yazabilir.
+    """
 
 
 with open("markalar.txt", "r", encoding="utf-8") as file:
--- 7.Dosya İşlemleri/dosyaya-yazma.py
+++ 7.Dosya İşlemleri/dosyaya-yazma.py
@@ -1,10 +1,10 @@
-# "w": (Write) yazma modu. 
-#    ** Dosyayı konumda oluşturur. 
+#    "w" : (Write) yazma modu. 
+#    ** Dosya konumda yoksa, oluşturur. 
 #    ** Eğer konumda aynı dosya varsa dosyayı siler ve yeni oluşturur.
 
 with open("dosya.txt","w",encoding="utf-8") as file:
-    file.write("Çınar Turan\n")
-    file.write("Efe Turan\n")
+    file.write("Eren Kozlu\n")
+    file.write("Fatih Gün\n")
 
 with open("dosya.txt","r",encoding="utf-8") as file:
     print(file.read())
