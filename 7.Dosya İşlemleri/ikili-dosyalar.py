29b9432676181e640d1fc87aa2dacabc5abc7d37
AhmetBedir <ahmet.bedir767@gmail.com>
2025-07-18 12:47:58 +0300

    güncelleme işlemi yapıldı.

--- 7.Dosya İşlemleri/ikili-dosyalar.py
+++ 7.Dosya İşlemleri/ikili-dosyalar.py
@@ -1,10 +1,10 @@
-with open("bilgi iletişim teknolojileri.txt","rb") as file:
+with open("bilgi-iletisim-teknolojileri.pdf","rb") as file:
     print(file.read(7))
     
     
-#
-with open("bilgi iletişim teknolojileri.txt","rb") as file:
+#PDF dosyasından bilgi alma.
+with open("bilgi-iletisim-teknolojileri.pdf","rb") as file:
     okunan = file.read()
     producer_index = okunan.index(b"/Producer")
-    print(producer_index)
-    print(okunan[14407:14419])
+    
+    print(okunan[producer_index:producer_index+80])
