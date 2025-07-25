58db4610116ad66d4e6fed4406505c78fa399c1c
Termux <ahmet.bedir767@gmail.com>
2025-07-25 15:27:57 +0300

    güncelleme işlemi yapıldı.

--- 7.Dosya İşlemleri/ikili-dosyalar/ikili-dosyalar.py
+++ 7.Dosya İşlemleri/ikili-dosyalar/ikili-dosyalar.py
@@ -9,16 +9,19 @@ with open("bilgi-iletisim-teknolojileri.pdf","rb") as file:
     
     print(okunan[producer_index:producer_index+80])
     
-###  
+###
 print('#'*40)
-with open("git-commands.pdf","rb") as f:
-    okunan = f.read()
-    index = okunan.index(b"Type")
-    print(okunan[index:index+15])
+
+liste = ["git-commands.pdf","git-commands.html","metin.txt"]
+for dosya in liste:
+    print(open(dosya,"rb").read(22))
+    print('---')
     
     
 ###
 print('#'*40)
-with open("git-commands.html","rb") as f:
-    okunan = f.read(52)
-    print(okunan)
+
+liste = ["git-commands.html","metin.txt"]
+for dosya in liste:
+    print(open(dosya,"r").read(22))
+    print('---')
