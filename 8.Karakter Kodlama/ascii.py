20e32862c8079819a76ea1c2cb1e89c508d313b9
Termux <ahmet.bedir767@gmail.com>
2025-07-26 19:31:19 +0300

    8.Karakter Kodlama/ascii.py dosyası eklendi.

---
+++ 8.Karakter Kodlama/ascii.py
@@ -0,0 +1,7 @@
+#ASCII Bilgi Alışverişi için Standart Amerikan Kodu ‘American Standard Code for Information Interchange’ tablosu yedi bitlik bir sistemdir.
+for i in range(128):
+    if i % 4 == 0:
+        print("\n")
+
+    print("{:<1}{:>7}\t".format(i, repr(chr(i))), sep="", end="")
+print("\n")
