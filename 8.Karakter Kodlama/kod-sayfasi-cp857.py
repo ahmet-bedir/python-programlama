757de6c1269736e0c29e7b36b910f5f438c69472
Termux <ahmet.bedir767@gmail.com>
2025-07-28 19:10:20 +0300

    güncelleme işlemi yapıldı.

--- 8.Karakter Kodlama/kod-sayfasi-cp857.py
+++ 8.Karakter Kodlama/kod-sayfasi-cp857.py
@@ -1,4 +1,4 @@
-#Microsoft şirketinin 1660'lı yıllarda Türkiye’ye gönderdiği bilgisayarlarda tanımlı ‘cp857’ adlı kod sayfasında 128 ile 256 aralığında Türkçe karakterlere de yer verilmiştir.
+#Microsoft şirketinin 1960'lı yıllarda Türkiye’ye gönderdiği bilgisayarlarda tanımlı ‘cp857’ adlı kod sayfasında ilk 127 karakter hariç (ilk 127 karakter ascii'dir), 128 ile 256 aralığında Türkçe karakterlere de yer verilmiştir.
 karakter = "Ç".encode("cp857") #Burada kullandığımız encode() metodu herhangi bir karakteri herhangi bir karakter kodlama sistemine göre kodlayabiliriz. Mesela örnekte ‘Ç’ harfini ‘cp857’ adlı kod sayfasına göre kodladık ve bunların bu kod sayfasında hangi sayıya karşılık geldiğini bulduk.
 print(karakter, type(karakter))
 
--- 8.Karakter Kodlama/kod-sayfasi.py
+++ 8.Karakter Kodlama/kod-sayfasi.py
@@ -1,4 +1,8 @@
 ######################################
+for i in range(1, 5):
+    print("{} bayt kullanırsak toplam 2**{:<2} = {:,} karakter kodlanabilinir.".format(i, i*8, (2**(i*8))))
+    
+    
 #print("Ç".encode("ascii")) #ASCII kodlama sisteminde 'Ç' harfi bulunmadığı için hata alırız.
 print("a".encode("ascii")) #b'a'
 print("a".encode("cp857")) #b'a'
@@ -8,5 +12,8 @@ print("Ç".encode("utf-8")) #b'\xc3\x87'
 print(int("c3",16)) #195
 print(int("87",16)) #135
 
-print("ß".encode("latin1"))
+print("ß".encode("latin1")) #b'\xdf'
 print(int("df",16)) #223
+
+print("€".encode("cp1254")) #b'\x80'
+print(int("80",16)) #128
