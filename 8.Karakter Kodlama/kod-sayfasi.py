e9923155bfe475d09e8a73860715cd0f02de052f
Termux <ahmet.bedir767@gmail.com>
2025-07-29 12:41:56 +0300

    güncelleme işlemi yapıldı.

--- 8.Karakter Kodlama/kod-sayfasi.py
+++ 8.Karakter Kodlama/kod-sayfasi.py
@@ -3,17 +3,25 @@ for i in range(1, 5):
     print("{} bayt kullanırsak toplam 2**{:<2} = {:,} karakter kodlanabilinir.".format(i, i*8, (2**(i*8))))
     
     
+harfler = "abcçdefgğhıijklmnoöprsştuüvyz"
+for s in harfler:
+    print("{:<5}{:<15}{} byte".format(s,str(s.encode("utf-8")), len(s.encode("utf-8"))))
+    
 #print("Ç".encode("ascii")) #ASCII kodlama sisteminde 'Ç' harfi bulunmadığı için hata alırız.
 print("a".encode("ascii")) #b'a'
 print("a".encode("cp857")) #b'a'
-
 print("a".encode("utf-8")) #b'a'
-print("Ç".encode("utf-8")) #b'\xc3\x87'
-print(int("c3",16)) #195
-print(int("87",16)) #135
 
+print("Ç".encode("utf-8")) #b'\xc3\x87'(c387 sayısı utf-8 kod çözücüsünde 'Ç' harfine karşılık gelir)
+sayi = (int("c387",16)) #50055
+uzunluk = sayi.bit_length()
+print(f"'Ç' harfi utf-8 kod çözücüsünde {uzunluk} bit, {uzunluk//8} bayt uzunluğundadır.")
+
+
+"""
 print("ß".encode("latin1")) #b'\xdf'
 print(int("df",16)) #223
 
 print("€".encode("cp1254")) #b'\x80'
 print(int("80",16)) #128
+"""
