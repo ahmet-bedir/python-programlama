cd422e21bd3fe29ff42959941d03d58155532658
Termux <ahmet.bedir767@gmail.com>
2025-07-31 19:44:57 +0300

    güncelleme işlemi yapıldı.

--- 7.Dosya İşlemleri/uyg.py
+++ 7.Dosya İşlemleri/uyg.py
@@ -6,6 +6,6 @@ liste = ["a\n","b\n","c\n","ç\n"]
 a.writelines(liste)
 a.close()
 """
-# cp1254 ile kodlanmış bir belgeyi UTF-8 ile açmaya kalkışırsanız veya siz hiçbir kod çözücü belirtmediğiniz halde kullandığınız işletim sistemi öntanımlı olarak dosyaları açmak için cp1254 harici bir kod çözücüyü kullanıyorsa, dosyayı okuma esnasında 'UnicodeDecodeError' hatası alırsınız.
-with open("metin.txt",encoding="utf-8") as dosya:
+# cp1254 ile kodlanmış bir belgeyi UTF-8 ile açmaya kalkışırsanız veya siz hiçbir kod çözücü belirtmediğiniz halde kullandığınız işletim sistemi öntanımlı olarak dosyaları açmak için cp1254 harici bir kod çözücüyü kullanıyorsa, dosyayı okuma esnasında 'UnicodeDecodeError' hatası alırsınız. Hata almamak için open foksiyonunun errors parametresini kullanmalısınız. 
+with open("metin.txt",encoding="utf-8",errors="replace") as dosya:
     print(dosya.read())
