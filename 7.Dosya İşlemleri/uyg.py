573ead526e363bab2cb478454ac732d3878c74ff
Termux <ahmet.bedir767@gmail.com>
2025-07-31 23:37:10 +0300

    güncelleme işlemi yapıldı.

--- 7.Dosya İşlemleri/uyg.py
+++ 7.Dosya İşlemleri/uyg.py
@@ -1,11 +1,12 @@
 """
 dosya = open("metin.txt","w",encoding="cp1254")
-dosya.write("ağaç\n")
+dosya.write("ağıt\n")
 dosya.seek(5)
 liste = ["a\n","b\n","c\n","ç\n"]
-a.writelines(liste)
-a.close()
+dosya.writelines(liste)
+dosya.close()
 """
 # cp1254 ile kodlanmış bir belgeyi UTF-8 ile açmaya kalkışırsanız veya siz hiçbir kod çözücü belirtmediğiniz halde kullandığınız işletim sistemi öntanımlı olarak dosyaları açmak için cp1254 harici bir kod çözücüyü kullanıyorsa, dosyayı okuma esnasında 'UnicodeDecodeError' hatası alırsınız. Hata almamak için open foksiyonunun errors parametresini kullanmalısınız. 
-with open("metin.txt",encoding="utf-8",errors="replace") as dosya:
+with open("metin.txt",encoding="utf-8",errors="ignore") as dosya:
     print(dosya.read())
+#'ignore' değerini kullanarak, Python’ın kodlanamayan karakterleri görmezden gelmesini sağlıyoruz. 'replace' değeri ise kodlanamayan karakterlerin yerine \ufffd karakterini yerleştirecektir. Bazı yerlerde bu karakteri elmas şeklinde siyah bir küp içine yerleştirilmiş soru işareti şeklinde görebilirsiniz.
