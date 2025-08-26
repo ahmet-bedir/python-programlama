e2c7f7c986e4b00390f58cd2e934b9eec45738a3
Termux <ahmet.bedir767@gmail.com>
2025-08-26 13:09:31 +0400

    güncelleme işlemi yapıldı.

--- 9.Sözlükler/sozlukler.py
+++ 9.Sözlükler/sozlukler.py
@@ -45,8 +45,10 @@ kisiler = {
     }
 }
 print(f"Memleket : {kisiler["Ahmet Özkoparan"]["Memleket"]}\nMeslek : {kisiler["Ahmet Özkoparan"]["Meslek"]}\nYaş : {kisiler["Ahmet Özkoparan"]["Yaş"]}")
+
 print("+-+"*48)
 ### Sözlüklere Öğe Eklemek
+#Bir değerin ‘anahtar’ olabilmesi için, o öğenin değiştirilemeyen (immutable) bir veri tipi olması gerekir. Python’da Demetler, Sayılar, Karakter Dizileri değiştirilemeyen veri tipleridir.
 sozluk = {}
 sozluk["a"] = 1
 sozluk[2] = "b"
@@ -56,7 +58,35 @@ sozluk["sözlük"] = {
     "soyad" : "bedir", 
     "yaş" : 35}
 sozluk["a"] = True
-sozluk[(1,2,3)] = 'a'
+sozluk[(1,2,3)] = 'd'
 print(sozluk)
 
+print("+-+"*48)
 ### Sözlük Öğeleri Üzerinde Değişiklik Yapmak
+sozluk['a'] = 555
+print(sozluk)
+
+print("+-+"*48)
+### Sözlük Üreteçleri (Dictionary Comprehensions)
+harfler = 'abcçdefgğhıijklmnoöprsştuüvyz'
+#
+sozluk = {}
+#for i in harfler:
+    #sozluk[i] = harfler.index(i)
+    
+#print(sozluk)
+
+#
+#for i in range(len(harfler)):
+    #sozluk[harfler[i]] = i
+    
+#print(sozluk)
+
+#
+sozluk = {i: harfler.index(i) for i in harfler}
+print(sozluk)
+
+#isim listesindeki isim ve kelime sayısını sözlük üreteçleri kullanarak bulma.
+isimler = ["ahmet", "mehmet", "fırat", "zeynep", "selma", "abdullah", "cem"]
+sozluk = {i:len(i) for i in isimler}
+print(sozluk)
