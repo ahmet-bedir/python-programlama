83ffa67ece8cdc3c74b0467fbcbc8a6f3e1cc2e7
Termux <ahmet.bedir767@gmail.com>
2025-08-26 21:14:26 +0400

    güncelleme işlemi yapıldı.

--- 9.Sözlükler/sozluk-metot.py
+++ 9.Sözlükler/sozluk-metot.py
@@ -1,1 +1,17 @@
-###
+# keys() metodu bir sözlüğün sadece anahtarlarını almak için kullanılır.
+sozluk = {"a": 0,
+          "b": 1,
+          "c": 2,
+          "d": 3}
+print(sozluk.keys())
+print(list(sozluk.keys()))
+print(tuple(sozluk.keys()))
+print(''.join(sozluk.keys()))
+print(str(sozluk.keys()))
+
+# values() metodu bir sözlüğün sadece değerlerini almak için kullanılır.
+print(sozluk.values())
+print(list(sozluk.values()))
+print(tuple(sozluk.values()))
+#print(''.join(sozluk.values()))
+print(str(sozluk.values()))
