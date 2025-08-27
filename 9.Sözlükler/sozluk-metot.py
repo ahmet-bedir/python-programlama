de6d2c011873a18757be21c5f3b1e93fe92e3838
Termux <ahmet.bedir767@gmail.com>
2025-08-27 19:56:13 +0400

    güncelleme işlemi yapıldı.

--- 9.Sözlükler/sozluk-metot.py
+++ 9.Sözlükler/sozluk-metot.py
@@ -7,11 +7,18 @@ print(sozluk.keys())
 print(list(sozluk.keys()))
 print(tuple(sozluk.keys()))
 print(''.join(sozluk.keys()))
-print(str(sozluk.keys()))
+print(str(sozluk.keys()), end='\n\n')
 
 # values() metodu bir sözlüğün sadece değerlerini almak için kullanılır.
 print(sozluk.values())
 print(list(sozluk.values()))
 print(tuple(sozluk.values()))
-print(''.join(str(sozluk.values())))
-print(str(sozluk.values()))
+print(''.join([str(i) for i in sozluk.values()]))
+print(str(sozluk.values()), end='\n\n')
+
+# items() metodu, bir sözlüğün hem anahtarlarını hem de değerlerini aynı anda almamızı sağlar:
+print(sozluk.items())
+
+#
+for anahtar, deger in sozluk.items():
+    print("{} = {}".format(anahtar, deger))
