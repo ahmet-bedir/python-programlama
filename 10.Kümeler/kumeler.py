4006068044da42df1dd22e0253b24168984bef10
AhmetBedir <ahmet.bedir767@gmail.com>
2025-10-07 14:40:54 +0300

    güncelleme işlemi yapıldı.

--- 10.Kümeler/kumeler.py
+++ 10.Kümeler/kumeler.py
@@ -1,3 +1,30 @@
-bos_kume = set()
+kume = set()
 
-print(bos_kume, type(bos_kume))
+print(kume, type(kume))
+
+##
+liste = ["elma","armut","erik"]
+kume = set(liste)
+print(kume, type(kume))
+
+##
+demet = ("ali","veli","ersin")
+kume = set(liste)
+print(kume, type(kume))
+
+##
+metin = "Python Programlama Dili."
+kume = set(metin)
+print(kume, type(kume))
+
+##
+sozluk = { 1 : "a",
+		   2 : "b",
+		   3 : "c"}
+kume = set(sozluk)
+print(kume, type(kume))
+
+## Sözlüklerin ayırt edici işareti olan süslü parantezleri kullanarak ve öğeleri birbirinden virgülle ayırarak da küme adlı veri tipini elde edebiliyoruz.
+kume = {'Python', 'C++', 'Ruby', 'PHP'}
+
+print(kume, type(kume))
