033e24df1a21404e16ec5816194e82397bf4bdfe
AhmetBedir <ahmet.bedir767@gmail.com>
2025-10-09 15:29:40 +0300

    güncelleme işlemi yapıldı.

---
+++ 11.Fonksiyonlar/fonksiyonlar.py
@@ -0,0 +1,35 @@
+def kayit(isim,soyisim,sistem,sehir):
+	print("-"*30)
+	print("İsim :", isim)
+	print("Soyisim :", soyisim)
+	print("İşletim Sistemi :", sistem)
+	print("Şehir :", sehir)
+	print("-"*30)
+
+kayit("ahmet","bedir","linux","kocaeli")
+kayit("ali","koz","windows","kars")
+kayit("ersin","şeker","unix","istanbul")
+
+
+###
+def sistem_blgisi():
+	import sys
+	print("\nSistemde kurulu Python'ın;")
+	print("\tana sürüm numarası:", sys.version_info.major)
+	print("\talt sürüm numarası:", sys.version_info.minor)
+	print("\tminik sürüm numarası:", sys.version_info.micro)
+	print("\nKullanılan işletim sisteminin;")
+	print("\tadı:", sys.platform)
+
+sistem_blgisi()
+
+###
+def uzunluk(kelime):
+	i = 0
+	for harf in kelime:
+		i=i+1
+	print(i)
+
+uzunluk("python")
+# diğer kodlar...
+uzunluk("ali")
