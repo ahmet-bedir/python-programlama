02a80206b9d431271138c54ea733b2b0dd4e577c
AhmetBedir <ahmet.bedir767@gmail.com>
2025-10-10 21:11:30 +0300

    güncelleme işlemi yapıldı.

---
+++ 11.Fonksiyonlar/return.py
@@ -0,0 +1,23 @@
+##
+def ornek():
+	return "deneme."
+
+a = ornek()
+print(a)
+
+##
+def ismin_ne():
+	isim = input("ismin ne? ")
+	return isim
+
+print("Merhaba {} . Nasılsın?".format(ismin_ne()))
+
+##
+def fonk(s):
+	if s < 0:
+		return "Eksi Değerli Sayı Olmamalı!"
+	else:
+		return s
+
+a = fonk(-1)
+print(a)
--- 11.Fonksiyonlar/uyg.py
+++ 11.Fonksiyonlar/uyg.py
@@ -1,7 +1,25 @@
+# Rastgele Sayıda İsimsiz Parametre Belirleme.
 def toplama(*sayilar):
 	toplam = 0
 	for sayi in sayilar:
 		toplam += sayi
 	print("Toplam :", toplam)
 
-toplama(1,2,3,4,5)
+toplama(1,2,3)
+
+toplama(2,2,2,4)
+
+# Rastgele Sayıda İsimli Parametre Belirleme.
+def fonksiyon(**parametreler):
+    print(parametreler)
+
+fonksiyon(isim="Ahmet", soyisim="Öz", meslek="Mühendis", şehir="Ankara")
+
+##
+def kayit(**bilgiler):
+	print("-"*30)
+	for anahtar, deger in bilgiler.items():
+	    print("{:<10} : {}".format(anahtar, deger))
+	print("-"*30)
+
+kayit(Ad="Fırat", Soyad="Özgül", Şehir="İstanbul", Tel="533 321 3232")
