cbc5bb1213b2d4fb822a08f49fd370b22315a138
AhmetBedir <ahmet.bedir767@gmail.com>
2025-10-15 21:05:31 +0300

    gömülü fonk dosyası oluşturuldu.

---
+++ 11.Fonksiyonlar/gomulu-fonk.py
@@ -0,0 +1,7 @@
+# all() fonksiyonu, bir dizi içinde bulunan bütün değerler True ise True değeri, eğer bu değerlerden herhangi biri False ise de False değeri döndürür.
+l1 = 12
+l2 = l1 == 12
+l3 = l1 > 11
+l4 = l1 % 3 == 0
+print(all([l1,l2,l3]))
+
