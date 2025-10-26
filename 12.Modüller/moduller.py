b22721d5b09451c82075925b18da27b74f34ee18
AhmetBedir <ahmet.bedir767@gmail.com>
2025-10-26 13:19:57 +0300

    güncelleme işlemi yapıldı.

--- 12.Modüller/moduller.py
+++ 12.Modüller/moduller.py
@@ -5,3 +5,10 @@ if os.name == 'posix':
 	print('Hoşgeldin Linux Kullanıcısı!')
 elif os.name == 'nt':
 	print('Hoşgeldin Linux Kullanıcısı!')
+
+
+# o anda hangi dizin altında bulunduğunuzu öğrenmek için:
+print(os.getcwd())
+
+# o anda içinde bulunduğunuz dizinde yeni bir dizin oluşturmak için:
+print(os.makedirs('DATA'))
