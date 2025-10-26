f0e4d61545cc5f1c27befbab82a8893262ed6b1a
AhmetBedir <ahmet.bedir767@gmail.com>
2025-10-26 17:13:46 +0300

    güncelleme işlemi yapıldı.

--- 12.Modüller/moduller.py
+++ 12.Modüller/moduller.py
@@ -1,4 +1,5 @@
-#
+"""
+# Modüller
 import os
 
 if os.name == 'posix':
@@ -7,12 +8,26 @@ elif os.name == 'nt':
 	print('Hoşgeldin Linux Kullanıcısı!')
 
 
-# o anda hangi dizin altında bulunduğunuzu öğrenmek için:
-print(os.getcwd())
 
-# o anda içinde bulunduğunuz dizinde yeni bir dizin oluşturmak için:
-#print(os.makedirs('DATA'))
+print(os.getcwd()) # o anda hangi dizin altında bulunduğunuzu öğrenmek için.
+
+
+print(os.makedirs('DATA')) # o anda içinde bulunduğunuz dizinde yeni bir dizin oluşturmak için.
+
 
 ### subprocess modülü, harici komutları Python içinden çalıştırabilmemizi sağlayan bir araçtır.
 import subprocess as sp
 sp.call(['bash','komut.sh'])
+
+
+### webbrowser modülü, bilgisayarımızda kurulu internet tarayıcısını kullanarak internet sitelerini açabilmemizi sağlar.
+import webbrowser as web
+web.open('www.duckduckgo.com')
+"""
+
+### import os gibi bir komutla bütün o isimleri içe aktarmak yerine, yalnızca kullanacağınız isimleri içe aktarmayı tercihde edebilirsiniz. Mesela os modülünün yalnızca name niteliğini kullanacaksanız:
+from os import name, listdir
+print(name)
+#print(getcwd()) #hata!
+print(listdir())
+
