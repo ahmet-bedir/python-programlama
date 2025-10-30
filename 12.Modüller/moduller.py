97b8c5b50a170be59388f0944fce167cb8ff1938
AhmetBedir <ahmet.bedir767@gmail.com>
2025-10-30 17:16:42 +0300

    güncelleme işlemi yapıldı.

--- 12.Modüller/moduller.py
+++ 12.Modüller/moduller.py
@@ -68,6 +68,16 @@ import prog
 print(prog.a)
 """
 
-### __doc__
+### Modüllerin Özel Nitelikleri
+import os, sys, random
+set_os = set(dir(os))
+set_sys = set(dir(sys))
+set_random = set(dir(random))
+print(set_os & set_sys & set_random)
+# Bu kodlar, os, sys ve random modüllerinin kesişim kümesini, yani her üç modülde ortak olarak bulunan nitelikleri verecektir:
+# {'__doc__', '__package__', '__loader__', '__name__', '__spec__'}
+
+
+### __doc__ Niteliği
 import sozluk ; print(sozluk.__doc__)
 
