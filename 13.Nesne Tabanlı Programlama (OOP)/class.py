ae0902e686e220dc3ddc52cb346931af8499b131
Termux <ahmet.bedir767@gmail.com>
2025-11-09 16:40:27 +0400

    güncelleme işlemi yapıldı.

--- 13.Nesne Tabanlı Programlama (OOP)/class.py
+++ 13.Nesne Tabanlı Programlama (OOP)/class.py
@@ -5,7 +5,7 @@ class Personal:
         print("Yapıcı metod!")
         
 nesne1 = Personal()
-'''
+
 
 ###
 class Personal:
@@ -19,3 +19,22 @@ p3 = Personal("mehmet",18)
 
 print(p1.ad, '-', p1.yas)
 print(p3.ad, '-', p3.yas)
+'''
+
+###
+class Personal:
+    def __init__(self,a,y):
+        self.ad = a
+        self.yas = y
+    def __str__(self):
+        return f"Kullanıcı Gözüyle:\nİsim : {self.ad}\nYaş : {self.yas}"
+    def __str__(self):
+        return f"Yazılımcı Gözüyle:\nİsim : {self.ad!r}\nYaş : {self.yas!r}"
+        
+p1 = Personal("ahmet",37)
+p2 = Personal("ali",27)
+p3 = Personal("mehmet",18)
+
+print(p1)
+print(p2)
+print(p3)
