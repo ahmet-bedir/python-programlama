caac5bf2709c5ef314ae24171867ad78a7fa85cf
Termux <ahmet.bedir767@gmail.com>
2025-11-18 12:06:45 +0300

    güncelleme işlemi yapıldı.

--- 13.Nesne Tabanlı Programlama (OOP)/consructor.py
+++ 13.Nesne Tabanlı Programlama (OOP)/consructor.py
@@ -3,6 +3,16 @@
 #   def __init__(self):
 #      print("Yapıcı metod!")
 #nesne1 = Personal() # sınıfın örneklendiği an yapıcı metod çalışır.
+
+class Deneme:
+    a = 5
+    def __init__(self):
+        self.b = 7
+
+
+ornek1 = Deneme()
+print(ornek1.a, ornek1.b)
+
 """
 ###
 class Urunler:
