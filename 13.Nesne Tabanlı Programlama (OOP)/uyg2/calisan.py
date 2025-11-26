aa502c953c7cb7c3d41db78426bb05beb4bc02e1
ahmet <ahmet@kali.kali.org>
2025-11-26 23:08:13 +0300

    güncelleme işlemi yapıldı.

--- 13.Nesne Tabanlı Programlama (OOP)/uyg2/calisan.py
+++ 13.Nesne Tabanlı Programlama (OOP)/uyg2/calisan.py
@@ -4,15 +4,19 @@ class Calisan:
         self.isim = isim
         self.kabiliyetleri = []
         self.personel_ekle()
+
     def personel_ekle(self):
         self.personel.append(self.isim)
         print(f"'{self.isim}' adlı kişi personel listesine eklendi.")
+
     def personel_listesi(self):
         print("== Personel Listesi ==")
         for kisi in self.personel:
             print(kisi)
+
     def kabiliyet_ekle(self,kabiliyet):
         self.kabiliyetleri.append(kabiliyet)
+        
     def kabiliyet_listesi(self):
         print(f"'{self.isim}' adlı personelin kabiliyetleri:")
         for k in self.kabiliyetleri:
