c8d7df5fbd0357a2a51e6f8abb01fc04799aa7f0
Termux <ahmet.bedir767@gmail.com>
2025-11-26 17:14:05 +0300

    güncelleme işlemi yapıldı.

--- 13.Nesne Tabanlı Programlama (OOP)/uyg2/ana_program.py
+++ 13.Nesne Tabanlı Programlama (OOP)/uyg2/ana_program.py
@@ -1,5 +1,15 @@
 import calisan as cal
 
 p1 = cal.Calisan("ali")
+p1.kabiliyet_ekle("üç harfli")
+p1.kabiliyet_ekle("cin gibi")
 
 p2 = cal.Calisan("erdi")
+p2.kabiliyet_ekle("liderlik vasfı")
+print('\n')
+
+p1.personel_listesi()
+print('\n')
+
+p1.kabiliyet_listesi()
+p2.kabiliyet_listesi()
--- 13.Nesne Tabanlı Programlama (OOP)/uyg2/calisan.py
+++ 13.Nesne Tabanlı Programlama (OOP)/uyg2/calisan.py
@@ -2,7 +2,18 @@ class Calisan:
     personel = []
     def __init__(self,isim):
         self.isim = isim
+        self.kabiliyetleri = []
         self.personel_ekle()
     def personel_ekle(self):
         self.personel.append(self.isim)
         print(f"'{self.isim}' adlı kişi personel listesine eklendi.")
+    def personel_listesi(self):
+        print("== Personel Listesi ==")
+        for kisi in self.personel:
+            print(kisi)
+    def kabiliyet_ekle(self,kabiliyet):
+        self.kabiliyetleri.append(kabiliyet)
+    def kabiliyet_listesi(self):
+        print(f"'{self.isim}' adlı personelin kabiliyetleri:")
+        for k in self.kabiliyetleri:
+            print(k)
