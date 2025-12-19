0a118ceda64e210b6c9fd129e80f0b92dbc1ebda
Termux <ahmet_bedir41@hotmail.com>
2025-12-19 11:54:25 +0300

    oop uyg3 eklendi.

---
+++ 13.Nesne Tabanlı Programlama (OOP)/uyg3/uyg.py
@@ -0,0 +1,5 @@
+liste = [('9789753424080', 'Greenberg', 'Sana Gül Bahçesi Vadetmedim', 'Metis'),
+         ('975872519X', 'Evren', 'Postmodern Bir Kız Sevdim', 'İthaki'),
+         ('9789754060409', 'Nietzsche', 'Böyle Buyurdu Zerdüşt', 'Cem')]
+for li in liste:
+    print(*li, sep=', ')
