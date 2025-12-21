4a1c7c0fb007e5be6462a838b00a660a5ec06099
Ahmet Bedir <ahmet.bedir767@gmail.com>
2025-12-21 17:18:23 +0300

    regex.py güncellendi.

--- 14.Paketler/regular/regex.py
+++ 14.Paketler/regular/regex.py
@@ -1,17 +1,24 @@
 import re
 
-print(dir(re))
+# print(dir(re))
 
 # match() metodu bir karakter dizisinde yalnızca en başında belirtilen kelimenin geçip geçmediğini öğrenmek için kullanılır.
 
 a = "python güçlü bir programlama dilidir."
-sorgu = re.match("python",a)
-print(sorgu)
 
-sorgu = re.match("hon",a)
-if sorgu == None:
-    print("Aranan kelime karakter dizisini en başında geçmemektedir!")
+# sorgu = re.match( "python",a)
+# if sorgu:
+#     print(f"'{sorgu.group()}' kelimesi karakter dizisinin en başında geçmektedir.")
+#     print("Konum:", sorgu.span())
+# else:
+#     print("Aranan kelime karakter dizisinin en başında geçmemektedir!")
+#
+# # alternatif
+# bol = a.split()
+# print(bol[0] == "python")
+# print(a.startswith("python"))
 
+# search() metodu karakter dizisinin genelinde belirtilen kelimenin geçip geçmediğini öğrenmek için kullanılır.
+sorgu = re.search("güçhhlü",a)
 
-sorgu = re.match("python",a)
-print(sorgu)
+print(sorgu.group())
