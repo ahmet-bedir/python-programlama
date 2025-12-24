128caeb82c339c05dc982210352e1fe315872048
Ahmet Bedir <ahmet.bedir767@gmail.com>
2025-12-24 12:57:35 +0300

    regex dosyası güncellendi.

--- 14.Paketler/regex/regex.py
+++ 14.Paketler/regex/regex.py
@@ -2,7 +2,7 @@ import re
 
 # print(dir(re))
 
-# match() metodu bir karakter dizisinde yalnızca en başında belirtilen kelimenin geçip geçmediğini öğrenmek için kullanılır.
+### match() metodu bir karakter dizisinde yalnızca en başında belirtilen kelimenin geçip geçmediğini öğrenmek için kullanılır.
 
 a = "python güçlü bir programlama dilidir."
 
@@ -18,7 +18,7 @@ a = "python güçlü bir programlama dilidir."
 # print(bol[0] == "python")
 # print(a.startswith("python"))
 
-# search() metodu karakter dizisinin genelinde belirtilen kelimenin geçip geçmediğini öğrenmek için kullanılır.
+### search() metodu karakter dizisinin genelinde belirtilen kelimenin geçip geçmediğini öğrenmek için kullanılır.
 # sorgu = re.search("güçlü",a)
 #
 # if sorgu != None:
@@ -34,3 +34,33 @@ a = "python güçlü bir programlama dilidir."
 #     if sorgu:
 #         print(f"'{sorgu.group()}' kelimesi liste içerisinde geçmektedir.")
 
+for i in dir(re):
+	sorgu = re.search("searcha",i)
+	if sorgu:
+		print("Bu metod mevcut.")
+
+### ﬁndall() metodu aranan kelimelefi liste şeklinde verir.
+metin = """Guido Van Rossum Python'ı geliştirmeye 1990 yılında başlamış... Yani
+aslında Python için nispeten yeni bir dil denebilir. Ancak Python'un çok uzun
+bir geçmişi olmasa da, bu dil öteki dillere kıyasla kolay olması, hızlı olması,
+ayrı bir derleyici programa ihtiyaç duymaması ve bunun gibi pek çok nedenden
+ötürü çoğu kimsenin gözdesi haline gelmiştir. Ayrıca Google'ın da Python'a özel
+bir önem ve değer verdiğini, çok iyi derecede Python bilenlere iş olanağı
+sunduğunu da hemen söyleyelim. Mesela bundan kısa bir süre önce Python'ın
+yaratıcısı Guido Van Rossum Google'de işe başladı..."""
+
+##
+# sorgu = re.findall("işe",metin)
+# i = 0
+# for eleman in sorgu:
+# 	i += 1
+# 	print(f"{i}.{eleman}")
+
+## search metodu ile...
+liste = metin.split()
+i = 0
+for eleman in liste:
+	sorgu = re.search("Python",eleman)
+	if sorgu:
+		i += 1
+		print(f"{i}.{sorgu.group()}")
