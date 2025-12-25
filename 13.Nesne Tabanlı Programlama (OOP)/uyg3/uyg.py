fe637e9b275b2f6c0165a5bf043c5c4359450f1d
Termux <ahmet_bedir41@hotmail.com>
2025-12-25 17:11:03 +0300

    oop de uyg yapılıyor.

--- 13.Nesne Tabanlı Programlama (OOP)/uyg3/uyg.py
+++ 13.Nesne Tabanlı Programlama (OOP)/uyg3/uyg.py
@@ -1,5 +1,41 @@
-liste = [('9789753424080', 'Greenberg', 'Sana Gül Bahçesi Vadetmedim', 'Metis'),
-         ('975872519X', 'Evren', 'Postmodern Bir Kız Sevdim', 'İthaki'),
-         ('9789754060409', 'Nietzsche', 'Böyle Buyurdu Zerdüşt', 'Cem')]
-for li in liste:
-    print(*li, sep=', ')
+#Bu program; isbn, isim, eser ve yayınevi ölçütlerine göre bu listeden veri alabilmemizi sağlayacak.
+liste = [('9789753424080',
+          'Greenberg',
+          'Sana Gül Bahçesi Vadetmedim',
+          'Metis'),
+         ('975872519X',
+          'Evren',
+          'Postmodern Bir Kız Sevdim',
+          'İthaki'),
+         ('9789754060409',
+          'Nietzsche',
+          'Böyle Buyurdu Zerdüşt',
+          'Cem')]
+
+def sorgula(ölçüt=None, değer=None):
+    for li in liste:
+        if not ölçüt and not değer:
+            print(*li, sep=', ')
+
+        elif ölçüt == 'isbn':
+            if değer == li[0]:
+                print(*li, sep=', ')
+
+        elif ölçüt == 'yazar':
+            if değer == li[1]:
+                print(*li, sep=', ')
+
+        elif ölçüt == 'eser':
+            if değer == li[2]:
+                print(*li, sep=', ')
+
+        elif ölçüt == 'yayınevi':
+            if değer == li[3]:
+                print(*li, sep=', ')
+                
+print('-'*45)              
+sorgula()
+print('-'*45)
+sorgula("isbn","975872519X")
+print('-'*45)
+sorgula(ölçüt="yayınevi",değer="Cem")
