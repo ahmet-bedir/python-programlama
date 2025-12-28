71e98ecf10e3b099b89e6dc0fce4e708236fd22d
Ahmet Bedir <ahmet.bedir767@gmail.com>
2025-12-28 16:55:14 +0300

    meta dosyası güncellendi.

--- 14.Paketler/regex/metakarakterler.py
+++ 14.Paketler/regex/metakarakterler.py
@@ -1,18 +1,30 @@
 import re
-liste = ["özcan deniz", "1mehmet", "85süleyman", "selim", "kemal", "özkan", "esra", "05dündar", "esin", "esma4", "özhan", "öz9lem", "özkan", "gR123"]
+
+### [] metakarakteri içinde gördüğü bütün karakterleri tek tek liste öğelerine uyguluyor. Önce “öz” ile başlayan bütün öğeleri alıyor, ardından “öz” hecesinden sonra “c” harﬁyle devam eden ve “an” hecesi ile biten öğeyi buluyor. Böylec “özcan” öğesini bulmuş oldu. Aynı işlemi, “öz” hecesinden sonra “h” harﬁni barındıran ve “an” hecesiyle biten öğeye uyguluyor. Bu şekilde ise “özhan” öğesini bulmuş oldu. En son hedef ise “öz” ile başlayıp “k” harﬁ ile devam eden ve “an” ile biten öğe. Yani listedeki “özkan” öğesi. En nihayetinde de elimizde “özcan”, “özhan” ve “özkan” öğeleri kalmış oluyor.
+liste = ["özcan", "mehmet", "süleyman", "selim", "kemal", "özkan", "esra", "dündar", "esin", "esma", "özhan", "özlem", "özkan"]
 # for l in liste:
 #     sorgu = re.search("öz[kch]an", l)
 #     if sorgu:
 #         print(sorgu.group())
-
-## sayı ile başlayanlar listelendi.
+#
+# print("-"*35)
 # for l in liste:
-#     if re.match("[0-9]", l):
-#     if re.match("[a-z][A-Z][0-9]",l):
+#     if re.search("es[mr]a",l):
 #         print(l)
 
-## . metakarakteri, yeni satır karakteri hariç bütün karakterleri temsil etmek için kullanılır.
+## sayı ile başlayanları listelemek için:
+# liste = ["23BH56","Ty76Z","4Y7UZ","TYUDZ","34534"]
+# for l in liste:
+#     if re.match("[0-9]", l):
+#         print(l) # 23BH56 4Y7UZ 34534
+
+# Düzenli ifademiz içinde geçen birinci [A-Z] ifadesi aradığımız karakter dizisi olan “Ty76Z” içindeki “T” harﬁni, ikinci [a-z] ifadesi “y” harﬁni, [0-9] ifadesi ise “7” sayısını temsil ediyor. Karakter dizisi içindeki geri kalan harﬂer ve sayılar otomatik olarak eşleştirilecektir. Listeden sadece “Ty76Z” öğesini alır.
+# for l in liste:
+#     if re.match("[A-Z][a-z][0-9]",l):
+#         print(l)
+
+### . metakarakteri, yeni satır karakteri hariç bütün karakterleri temsil etmek için kullanılır. “.” metakarakteri sadece tek bir karakterin yerini tutuyor.
 for l in liste:
-    sorgu = re.search("es.a",l)
+    sorgu = re.match("es.a",l)
     if sorgu:
         print(sorgu.group())
