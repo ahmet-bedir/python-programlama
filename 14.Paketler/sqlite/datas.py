248f9ad16d1aa137cc0e7eecdc248ea28e4946d9
Ahmet Bedir <ahmet.bedir767@gmail.com>
2026-02-01 13:54:00 +0300

    güncelleme işlemi yapıldı.

--- 14.Paketler/sqlite/datas.py
+++ 14.Paketler/sqlite/datas.py
@@ -1,8 +1,8 @@
 import sqlite3, os
 
-dosya = 'kitaplar.sqlite'
-dosya_mevcutmu = os.path.exists(dosya)
-print(dosya_mevcutmu)
+# dosya = 'kitaplar.sqlite'
+# dosya_mevcutmu = os.path.exists(dosya)
+# print(dosya_mevcutmu)
 
 with sqlite3.connect("kitaplar.sqlite") as vt:
     im = vt.cursor()
@@ -10,11 +10,11 @@ with sqlite3.connect("kitaplar.sqlite") as vt:
     sql = "SELECT * FROM kitaplar"
     im.execute(sql)
     veriler = im.fetchall()
-    print('-'*158)
+    print('+' + '-'*3 + '+' + '-'*50 + '+' + '-'*50 + '+' + '-'*50 + '+')
     print("|{:<3}|{:^50}|{:^50}|{:^50}|".format("No", "Kitap Adı", "Yazar", "Fiyat"))
-    print('-'*158)
+    print('+' + '-'*3 + '+' + '-'*50 + '+' + '-'*50 + '+' + '-'*50 + '+')
     i = 0
     for satir in veriler:
         i += 1
         print("|{:<3}|{:<50}|{:<50}|{:<50}|".format(i, satir[0], satir[1], satir[2]))
-    print('-'*158)
+    print('+' + '-'*3 + '+' + '-'*50 + '+' + '-'*50 + '+' + '-'*50 + '+')
