f06b95101fe2687c0cddab539225b192a0767385
Ahmet Bedir <ahmet.bedir767@gmail.com>
2026-02-05 23:24:37 +0300

    güncelleme işlemi yapıldı.

--- 14.Paketler/mysql/mysql-connect.py
+++ 14.Paketler/mysql/mysql-connect.py
@@ -1,1 +1,11 @@
-import mysql.connector
+import mysql.connector as my
+
+baglanti = my.connect(
+    host = "127.0.0.1", # 192.23.45.56
+    user = "ahmet",
+    password = "a",
+    database = "db"
+)
+im = baglanti.cursor()
+
+print(im)
