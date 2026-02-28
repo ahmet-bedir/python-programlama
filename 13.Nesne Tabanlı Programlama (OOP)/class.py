905d127462ce3be5a4e6b5500be318dc1e07cbca
Ahmet Bedir <ahmet.bedir767@gmail.com>
2026-02-28 23:00:16 +0300

    class.py dosyası güncellendi.

--- 13.Nesne Tabanlı Programlama (OOP)/class.py
+++ 13.Nesne Tabanlı Programlama (OOP)/class.py
@@ -1,10 +1,39 @@
-class Person:
-	def __init__(self, name):
-		self.name = name
-		print("Yapıcı Metod (constructor metod) Çalıştı.")
+class PersonClass:
+    # class attributes
+    city = "izmit"
+    
+    # constructor methods
+    def __init__(self, name, year):
+		# object attributes
+        self.name = name
+        self.year = year
+        print("Yapıcı Metod (constructor metod) Çalıştı.")
+        
+    # instance methods
+    def introFunc(self):
+        print("Selam, " + self.name)
+        
+    def ageCalc(self):
+        return 2026 - self.year
 
-object1 = Person("ali")
-print(object1.name)
+print('-'*40)
+# object (instance)
+object1 = PersonClass("ali",1988)
+# accessing object attributes
+print(f"İsim: {object1.name}\nDoğum Yılı: {object1.year}\nŞehir: {object1.city}")
+object1.introFunc()
+print("Yaş:", object1.ageCalc())
 
-object2 = Person("aylin")
-print(object2.name)
+print('-'*40)
+object2 = PersonClass(year=2002,name="aylin")
+print(f"İsim: {object2.name}\nDoğum Yılı: {object2.year}\nŞehir: {object2.city}")
+object2.introFunc()
+print("Yaş:", object2.ageCalc())
+
+print('-'*40)
+# updating
+object1.name = "ahmet"
+object1.city = "ankara"
+
+print(f"İsim: {object1.name}\nŞehir: {object1.city}")
+object1.introFunc()
