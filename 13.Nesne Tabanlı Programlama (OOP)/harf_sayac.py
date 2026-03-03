493eb2891d9da7c35be5977b1b153fead712f4af
Termux <ahmet.bedir767@gmail.com>
2026-03-03 12:54:26 +0300

    güncelleme işlemi yapıldı.

--- 13.Nesne Tabanlı Programlama (OOP)/harf_sayac.py
+++ 13.Nesne Tabanlı Programlama (OOP)/harf_sayac.py
@@ -1,3 +1,32 @@
+"""
+oop kullanılmadan önce:
+
+sesli_harfler = 'aeıioöuü'
+sayac = 0
+
+def kelime_sor():
+    return input('Bir kelime girin: ')
+
+def seslidir(harf):
+    return harf in sesli_harfler
+
+def artir(sayac, kelime):
+    for harf in kelime:
+        if seslidir(harf):
+            sayac += 1
+    return sayac
+
+def ekrana_bas(kelime):
+    mesaj = "{} kelimesinde {} sesli harf var."
+    print(mesaj.format(kelime, artir(sayac, kelime)))
+
+def calistir():
+    kelime = kelime_sor()
+    ekrana_bas(kelime)
+
+calistir()
+
+"""
 class HarfSayaci:
     def __init__(self):
         self.sesli_harfler = 'aeıioöuü'
