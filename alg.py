ebe914491e7751758405798dd2214010b2fe7d2b
Ahmet Bedir <ahmet.bedir767@gmail.com>
2026-03-08 21:45:13 +0300

    algorrithm

---
+++ alg.py
@@ -0,0 +1,22 @@
+# top = 0
+# i = 1
+
+# while i < 11:
+#     if i % 2 == 1:
+#         print(i)
+#         top += i
+#     i += 1
+    
+# print("Top:", top)
+
+top = 0
+i = 1
+
+while i < 11:
+    print(i)
+    top += i
+    i += 2
+    
+print("Top:", top)
+
+###########################################
