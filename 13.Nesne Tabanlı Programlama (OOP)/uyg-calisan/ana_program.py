fee6a79064e1402cbd6390d7ffa5e63bd3a2c63c
Termux <ahmet.bedir767@gmail.com>
2026-03-18 23:42:16 +0300

    güncelleme işlemi yapıldı.

--- 13.Nesne Tabanlı Programlama (OOP)/uyg-calisan/ana_program.py
+++ 13.Nesne Tabanlı Programlama (OOP)/uyg-calisan/ana_program.py
@@ -1,22 +1,29 @@
 import calisan_modulu as cal
-from calisan_modulu import Calisan
+#from calisan_modulu import Calisan
 
+print('='*45)
 print("calisan_modulu.py dosyasının içeriği:", dir(cal), sep='\n')
-print("Calisan sınıfının içeriği:", dir(Calisan), sep='\n')
+print('='*45)
+print("calisan_modulu.py dosyasındaki Calisan sınıfının içeriği:", dir(cal.Calisan), sep='\n')
 
-Calisan.personel_sayisi()
+print('='*45)
+cal.Calisan.personel_sayisi()
 
 p1 = cal.Calisan("ali")
-p1.kabiliyet_ekle("üç harfli")
-p1.kabiliyet_ekle("cin gibi")
+p1.kabiliyet_ekle("bilişim")
+p1.kabiliyet_ekle("iletişim")
 
 p2 = cal.Calisan("erdi")
 p2.kabiliyet_ekle("liderlik vasfı")
 print('\n')
 
-Calisan.personel_listesi()
-Calisan.personel_sayisi()
+print('='*45)
+
+cal.Calisan.personel_listesi()
+cal.Calisan.personel_sayisi()
 print('\n')
 
+print('='*45)
 p1.kabiliyet_listesi()
+print('='*45)
 p2.kabiliyet_listesi()
