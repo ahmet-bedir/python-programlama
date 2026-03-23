f45e2e1e3488d880ddf08f4463289f2fb7613a4e
Ahmet Bedir <ahmet.bedir767@gmail.com>
2026-03-23 23:10:36 +0300

    mysql güncellendi.

--- 14.Paketler/mysql/mysql.py
+++ 14.Paketler/mysql/mysql.py
@@ -6,8 +6,9 @@ class Urun:
             host = "127.0.0.1",
             user = "ahmet",
             password = "a",
-            database = "db"
-        )
+            database = "db")
+        
+    ###
     def urunEkle(self, urun_adi, fiyat, url, aciklama):
         self.urun_adi = urun_adi
         self.fiyat = fiyat
@@ -28,7 +29,8 @@ class Urun:
         finally:
             self.baglanti.close()
             print('database bağlantısı kapandı.')
-
+            
+    ###
     def cokUrunEkle(self, liste):
         self.liste = liste
 
@@ -46,21 +48,57 @@ class Urun:
         finally:
             self.baglanti.close()
             print('database bağlantısı kapandı.')
-
+            
+    ###
     def urunListesi(self):
         imlec = self.baglanti.cursor()
-        sql = """SELECT * FROM urunler;"""
+        sql = """SELECT * FROM urunler;""" #Tüm kayıtları çekmek istediğimiz için * kullanırız.
+        sql = """SELECT * FROM urunler ORDER BY urun_adi DESC;""" #urun_adi'na göre azalan sırada sıralamak istediğimiz için ORDER BY kullanırız. Ve DESC ile azalan sırada sıralarız. Eğer artan sırada sıralamak istiyorsak ASC kullanırız.
         imlec.execute(sql)
-        liste = imlec.fetchall() #fetchall fonksiyonu, tüm kayıtları liste(list) olarak döndürür.
-        for i in liste:
-            print(i)
-        # tekKayit = imlec.fetchone() #fetchone fonksiyonu, tek bir kayıt döndürür.
-        # print(tekKayit)
-
+        try:
+            liste = imlec.fetchall() #fetchall fonksiyonu, tüm kayıtları liste(list) olarak döndürür.
+            for i in liste:
+                print(i)
+                
+            # tekKayit = imlec.fetchone() #fetchone fonksiyonu, tek bir kayıt döndürür. Eğer birden fazla kayıt varsa, ilk kaydı döndürür. Eğer hiç kayıt yoksa, None döndürür.
+            # print(tekKayit)
+        except mysql.Error as err:
+            print('hata:', err)
+        finally:
+            self.baglanti.close()
+            
+    ###
+    def urunId(self, id):
+        imlec = self.baglanti.cursor()
+        sql = """Select * From urunler Where id=%s""" #id'ye göre kayıt çekmek istediğimiz için, id'yi parametre olarak alırız. Ve sql sorgusunda %s ile id'yi belirtiriz.
+        urunId = (id,) #id'yi demet(tuple) olarak tanımlarız. Çünkü execute fonksiyonu, değer(value) olarak demet(tuple) alır.
+        imlec.execute(sql,urunId)
+        sonuc = imlec.fetchone()
+        print(f'id: {sonuc[0]}\nÜrün adı: {sonuc[1]}\nFiyat: {sonuc[2]}')
         self.baglanti.close()
+        
+    ###
+    def urunBilgisi(self):
+        imlec = self.baglanti.cursor()
+
+        sql = """SELECT COUNT(*) FROM urunler;""" #urunler tablosundaki kayıt sayısını çekmek istediğimiz için COUNT(*) kullanırız.
+        sql = """SELECT AVG(fiyat) FROM urunler;""" #urunler tablosundaki fiyat ortalamasını çekmek istediğimiz için AVG() kullanırız.
+        # sql = """SELECT SUM(fiyat) FROM urunler;""" #urunler tablosundaki fiyat toplamını çekmek istediğimiz için SUM() kullanırız.
+        # sql = """SELECT MIN(fiyat) FROM urunler;""" #urunler tablosundaki en düşük fiyatı çekmek istediğimiz için MIN() kullanırız.
+        # sql = """SELECT MAX(fiyat) FROM urunler;""" #urunler tablosundaki en yüksek fiyatı çekmek istediğimiz için MAX() kullanırız.
+        # sql = """SELECT urun_adi,fiyat FROM urunler WHERE fiyat = (SELECT MAX(fiyat) FROM urunler);""" #urunler tablosundaki en yüksek fiyata sahip ürünleri çekmek istediğimiz için bu sorguyu kullanırız.
+
+        imlec.execute(sql)
+
+        sonuc = imlec.fetchone()    
+
+        print(f'Sonuç: {sonuc[0]}')
+        self.baglanti.close()
+
+        
 ###
-nesneUrunEkle = Urun()
-nesneUrunEkle.urunEkle("dell laptop",21000,"dell.jpg","dizüstü bilgisayar")
+# nesneUrunEkle = Urun()
+# nesneUrunEkle.urunEkle("dell laptop",21000,"dell.jpg","dizüstü bilgisayar")
 
 ###
 # liste = [
@@ -75,3 +113,11 @@ nesneUrunEkle.urunEkle("dell laptop",21000,"dell.jpg","dizüstü bilgisayar")
 ###
 nesneUrunleriListele = Urun()
 nesneUrunleriListele.urunListesi()
+
+###
+nesneUrunId = Urun()
+nesneUrunId.urunId(13)
+
+###
+nesneUrunBilgisi = Urun()
+nesneUrunBilgisi.urunBilgisi()
