955a6be3d4577113343af6ce5ff0d31d34ba4e54
Termux <ahmet.bedir767@gmail.com>
2026-03-24 09:43:28 +0300

    güncelleme işlemi yapıldı.

---
+++ 13.Nesne Tabanlı Programlama (OOP)/class-members/public-members/main.py
@@ -0,0 +1,13 @@
+import public
+
+s = public.Sinif()
+
+print(dir(s))
+
+s.ornek_metodu()
+s.sinif_metodu()
+s.statik_metot()
+print(s.sinif_niteligi)
+'''
+dir() fonksiyonunun çıktısında görünen ve normal yollardan erişebildiğimiz bütün bu öğeler birer aleni üyedir.
+'''
---
+++ 13.Nesne Tabanlı Programlama (OOP)/class-members/public-members/public.py
@@ -0,0 +1,13 @@
+class Sinif():
+    sinif_niteligi = 'sınıf niteliği'
+
+    def ornek_metodu(self):
+        print('örnek metodu')
+
+    @classmethod
+    def sinif_metodu(cls):
+        print('sınıf metodu')
+
+    @staticmethod
+    def statik_metot():
+        print('statik metot')
