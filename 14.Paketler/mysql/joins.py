0b259fe862ccbbbea9f95489fde4a060b812b401
Ahmet Bedir <ahmet.bedir767@gmail.com>
2026-04-10 22:22:10 +0300

    güncellendi.

--- 14.Paketler/mysql/joins.py
+++ 14.Paketler/mysql/joins.py
@@ -11,12 +11,14 @@ dbCursor = dbConnect.cursor()
 # sql = """SELECT * FROM products INNER JOIN categories ON products.categoryid = categories.category_id;"""
 # sql = """SELECT products.product_name, products.price, products.product_image, products.description, categories.category_name FROM products INNER JOIN categories ON products.categoryid = categories.category_id;"""
 # sql = """SELECT products.product_name, products.price, products.product_image, products.description, categories.category_name FROM products INNER JOIN categories ON products.categoryid = categories.category_id WHERE categories.category_name = 'telefon';"""
-sql = """SELECT p.product_name, p.price, p.product_image, p.description, c.category_name FROM products AS p INNER JOIN categories AS c ON p.categoryid = c.category_id WHERE c.category_name = 'telefon';"""
+sql = """SELECT p.product_name, p.price, p.product_image, p.description, c.category_name
+FROM products AS p INNER JOIN categories AS c ON p.categoryid = c.category_id
+WHERE c.category_name = 'telefon';"""
 
 
 dbCursor.execute(sql)
 try:
-    result = dbCursor.fetchall()    
+    result = dbCursor.fetchall()
     for product in result:
         print(product)
 except mysql.connector.Error as err:
--- p.py
+++ p.py
@@ -1,2 +1,2 @@
-
-
+status = "Geçti" if 11<2 else "Kaldı"
+print(f"{status}")
