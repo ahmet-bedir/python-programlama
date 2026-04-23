4cf0b26e087d610c6d3e44c758ee8a6c9eb18e50
Ahmet Bedir <ahmet.bedir767@gmail.com>
2026-04-23 13:40:10 +0300

    01.Giriş dizini olarak değiş

--- p.py
+++ p.py
@@ -1,10 +1,1 @@
-def bilgi_goster(ad, yas, sehir):
-    print(f"{ad}, {yas} yaşında, {sehir}'da yaşıyor.")
 
-# Keyword: sıra önemli değil!
-bilgi_goster(yas=25, sehir="İstanbul", ad="Ahmet")
-# Ahmet, 25 yaşında, İstanbul'da yaşıyor. ✅
-
-# İkisini karıştırabilirsin (ama positional önce gelmeli)
-bilgi_goster("Ahmet", 6, sehir="izmr")
-# Ahmet, 25 yaşında, İstanbul'da yaşıyor. ✅
