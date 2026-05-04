d66e81a55e85b46cf07bf362e94c0e718f4a94b2
Termux <ahmet.bedir767@gmail.com>
2026-05-04 11:24:49 +0300

    güncelleme işlemi yapıldı.

--- 01.Giriş/degisken-tanimlama.py
+++ 01.Giriş/degisken-tanimlama.py
@@ -1,17 +1,17 @@
-###
-print("-" * 45)
+### Çoklu Atama - Tek satırda birden fazla değişken atama.
+
 x, y, z = 1, "2", 3.0
 
 print(x, type(x))
 print(y, type(y))
-print(z, type(z), "\n")
+print(z, type(z))
 
+# Aynı Değeri Birden Fazla Değişkene Atama
 x = y = z = 4 #x, y, z = 4, 4, 4
 print(x, y, z)
 
 #################################################
 
-print("-" * 45)
 import keyword
 print("Kullanabileceğimiz Fonksiyonlar (keyword) :\n", dir(keyword), sep="", end="\n\n")
 
@@ -19,7 +19,6 @@ a = keyword.kwlist
 print("Tanımlayamayacağımız Değişken İsimleri :\n", a, sep="")
 print("-" * 45)
 print("Yasaklı Kelime Adedi :", len(a))
-print("-" * 45, end="\n\n")
 
 ###
 import keyword
@@ -56,20 +55,16 @@ pi = 3.14159
 alan = pi * yari_cap * yari_cap
 cevre = 2 * pi * yari_cap
 
-print(45 * "-")
 print("Yarıçapı " + str(int(yari_cap)) + " Olan Dairenin Alanı : " + str(alan))
 print("Yarıçapı", int(yari_cap) ,"Olan Dairenin Çevresi :", cevre)
-print(45 * "-", end="\n\n")
 
 ################################################
 
-print(45 * "-")
 print("İki üzeri üç :", 2**3) #8
 print("Üç üzeri iki :", pow(3,2)) #9
 print("Dörtün karekökü :", pow(4,0.5)) #2
 print("Üç üzeri iki bölü iki kalan :", pow(3,2,2)) #1
 print("Üç üzeri iki bölü üç kalan :", pow(3,2,3)) #0
-print("-" * 45, end="\n\n")
 
 ################################################
 
@@ -82,13 +77,11 @@ mart_fatura_tutari = 273.87
 birim_fiyat = mart_fatura_tutari / mart_aylik_sarfiyat
 gunluk_sarfiyat = mart_aylik_sarfiyat / mart
 nisan_fatura = nisan * gunluk_sarfiyat * birim_fiyat
-print("-" * 45)
+
 print("Nisan Ayı Faturası : ", nisan_fatura)
-print("-" * 45, end="\n\n")
 
 ################################################
 
-print("-" * 45)
 osman = "Araştırma Geliştirme Müdürü"
 mehmet = "Proje Sorumlusu"
 
@@ -99,4 +92,3 @@ print("·—" * 22)
 osman, mehmet = mehmet, osman
 print("Osman :", osman)
 print("Mehmet :", mehmet)
-print("-" * 45)
