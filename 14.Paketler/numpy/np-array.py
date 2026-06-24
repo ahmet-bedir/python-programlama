97a37a1b519892517717901782211935762dd7cb
AhmetBedir <ahmet.bedir767@gmail.com>
2026-06-24 15:48:51 +0300

    Otomatik güncelleme işlemi yapıldı.

--- 14.Paketler/numpy/np-array.py
+++ 14.Paketler/numpy/np-array.py
@@ -8,8 +8,13 @@ import numpy as np
 # print(np.__version__)
 
 ###
-array0 = np.array(9)
+array0 = np.array(9)  # 0D - Scalar (sıfır boyutlu)
 print(array0)
 
-array0 = np.array(9)
-print(array0)
+array1 = np.array([23,45,43,26])  # 1D (tek boyutlu array)
+print(array1)
+
+array2 = np.array(
+    [9,2,5],
+    [8,3,2])
+print(array2)
