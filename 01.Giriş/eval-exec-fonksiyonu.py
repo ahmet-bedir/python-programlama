e81a1b617c312397e916a6a4368b28dc70bcae1b
AhmetBedir <ahmet.bedir767@gmail.com>
2026-06-24 16:47:01 +0300

    Otomatik güncelleme işlemi yapıldı.

--- 01.Giriş/eval-exec-fonksiyonu.py
+++ 01.Giriş/eval-exec-fonksiyonu.py
@@ -1,46 +1,49 @@
 ###
 #print(eval("a = 3")) #hata.!
 
-exec("a = 3")
-print("a =", a)
+# exec("a = 3")
+# print("a =", a)
 
 
-###
-i = '7 * 3'
-print("7 * 3 =", eval(i))
+# ###
+# i = '7 * 3'
+# print("7 * 3 =", eval(i))
 
-i = 'print("Hello Python.!")'
-islem = eval(i)
-print(islem)
+# i = 'print("Hello Python.!")'
+# islem = eval(i)
+# print(islem)
 
-exec("a = '#'; b = 15; print(a*b)")
+# exec("a = '#'; b = 15; print(a*b)")
 
 
     
-###
-d1 = """
-Python'da ekrana çıktı verebilmek için 
-print() adlı bir fonksiyondan yararlanıyoruz...
-Bu fonksiyonu şöyle kullanabilirsiniz:
+# ###
+# d1 = """
+# Python'da ekrana çıktı verebilmek için 
+# print() adlı bir fonksiyondan yararlanıyoruz...
+# Bu fonksiyonu şöyle kullanabilirsiniz:
 
->>> print("Merhaba Dünya")
+# >>> print("Merhaba Dünya")
 
-Şimdi de aynı kodu siz yazın!
+# Şimdi de aynı kodu siz yazın!
 
->>> """
+# >>> """
 
-girdi = input(d1)
+# girdi = input(d1)
 
-exec(girdi)
+# exec(girdi)
 
-d2 = """
-Gördüğünüz gibi print() fonksiyonu,
-kendisine parametre olarak verilen
-değerleri ekrana basıyor... 
+# d2 = """
+# Gördüğünüz gibi print() fonksiyonu,
+# kendisine parametre olarak verilen
+# değerleri ekrana basıyor... 
 
-Böylece ilk dersimizi tamamlamış olduk.
-Şimdi bir sonraki dersimize geçebiliriz...
-"""
+# Böylece ilk dersimizi tamamlamış olduk.
+# Şimdi bir sonraki dersimize geçebiliriz...
+# """
 
-print(d2)
+# print(d2)
 
+data = eval(input("Giriş: "))
+
+print(data)
