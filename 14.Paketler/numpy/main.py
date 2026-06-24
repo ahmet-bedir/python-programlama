c76d38d22b7fca9c44fb3fb3d1886d1a7708403b
AhmetBedir <ahmet.bedir767@gmail.com>
2026-06-24 15:33:42 +0300

    Otomatik güncelleme işlemi yapıldı.

--- 14.Paketler/numpy/main.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-array_list = np.array([1, 2, 3, 4, 5, True, False, 7.7])
-array_list += 1
-
-print(array_list)
-print(type(array_list))
-print(np.__version__)
---
+++ 14.Paketler/numpy/np-array.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# array_list = np.array([1, 2, 3, 4, 5, True, False, 7.7])
+# array_list += 1
+
+# print(array_list)
+# print(type(array_list))
+# print(np.__version__)
+
+###
+array0 = np.array(9)
+print(array0)
+
+array0 = np.array(9)
+print(array0)
