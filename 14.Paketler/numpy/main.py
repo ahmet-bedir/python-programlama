a89b5298e55f046e10ec2833a918cd83a0203871
AhmetBedir <ahmet.bedir767@gmail.com>
2026-06-24 13:24:44 +0300

    Otomatik güncelleme işlemi yapıldı.

--- 14.Paketler/numpy/main.py
+++ 14.Paketler/numpy/main.py
@@ -1,5 +1,8 @@
 import numpy as np
 
-array_list = np.array([1, 2, 3, 4, 5])
+array_list = np.array([1, 2, 3, 4, 5, True, False, 7.7])
 array_list += 1
+
 print(array_list)
+print(type(array_list))
+print(np.__version__)
