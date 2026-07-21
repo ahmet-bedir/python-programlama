cc30a2a7c773bd22adc950c6d99f3a1198b8fa58
AhmetBedir <ahmet.bedir767@gmail.com>
2026-07-21 18:36:01 +0300

    Otomatik güncelleme işlemi yapıldı.

--- 14.Paketler/numpy/np-array.py
+++ 14.Paketler/numpy/np-array.py
@@ -8,24 +8,31 @@ import numpy as np
 # print(np.__version__)
 
 ###
-array0 = np.array(9)  # 0D - Scalar (sıfır boyutlu)
-print(array0)
-print(f"Kaç boyutlu? {array0.ndim}")
+# array0 = np.array(9)  # 0D - Scalar (sıfır boyutlu)
+# print(array0)
+# print(f"Kaç boyutlu? {array0.ndim}")
 
-array1 = np.array([23,45,43,26])  # 1D (tek boyutlu array)
-print(array1)
-print(f"Kaç boyutlu? {array1.ndim}")
+# array1 = np.array([23,45,43,26])  # 1D (tek boyutlu array)
+# print(array1)
+# print(f"Kaç boyutlu? {array1.ndim}")
 
-array2 = np.array([
-    [9,2,5],
-    [8,3,2]])  # 2D (iki boyutlu array)
-print(array2)
-print(f"Kaç boyutlu? {array2.ndim}")
+# array2 = np.array([
+#     [9,2,5],
+#     [8,3,2]])  # 2D (iki boyutlu array)
+# print(array2)
+# print(f"Kaç boyutlu? {array2.ndim}")
 
-array3 = np.array([
-    [[9,2,5],[8,3,2]],
-    [[7,3,4],[2,5,3]],
-    [[3,6,9],[1,4,5]]
-    ])  # 3D (üç boyutlu array)
-print(array3)
-print(f"Kaç boyutlu? {array3.ndim}")
+# array3 = np.array([
+#     [[9,2,5],[8,3,2]],
+#     [[7,3,4],[2,5,3]],
+#     [[3,6,9],[1,4,5]]
+#     ])  # 3D (üç boyutlu array)
+# print(array3)
+# print(f"Kaç boyutlu? {array3.ndim}")
+
+###########
+array_list = np.array([1, 3, 5, 7, 9], ndmin=3)
+print(array_list)
+
+array_list = np.arange(5, 16, 1)
+print(array_list)
