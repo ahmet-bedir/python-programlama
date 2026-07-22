a4ac154776c5697c1ba8901ebcf65d217871af08
AhmetBedir <ahmet.bedir767@gmail.com>
2026-07-22 07:49:46 +0300

    numpy kütüphanesi işlendi.

--- 14.Paketler/numpy/np-data-type.py
+++ 14.Paketler/numpy/np-data-type.py
@@ -6,7 +6,7 @@ print(arr.dtype)  # int64 (=8byte) (varsayılan uzunluk)
 arr = np.array(1, dtype=np.int8)
 print(arr.dtype)  # int8 (=1byte) (uzunluğu kendimiz belirledik)
 
-arr = np.array([1.22,5.38,8.41], dtype=np.float32)
+arr = np.array([1.22, 5.38, 8.41], dtype=np.float32)
 print(arr.dtype)  # int8 (=1byte)
 
 arr = np.array("ananaslar")
@@ -14,3 +14,6 @@ print(arr.dtype)  # <U9
 
 # tür dönüşümü [float->int]
 arr = np.array([4.32, 6.41, 8.4])
+new_arr = arr.astype(int)
+print(new_arr, new_arr.dtype)  # [4 6 8] int64
+
