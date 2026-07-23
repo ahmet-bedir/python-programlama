85bf1de3d8cc961280141f10c7d08f6209f8fa0e
AhmetBedir <ahmet.bedir767@gmail.com>
2026-07-23 19:12:51 +0300

    Otomatik güncelleme işlemi yapıldı.

--- p.py
+++ p.py
@@ -4,7 +4,9 @@ from PyQt6.QtWidgets import QApplication, QMainWindow
 def main():
     app = QApplication(sys.argv)
     win = QMainWindow()
+    win.setWindowTitle("Başlık")
+    win.setGeometry(1500,500,1000,700)
     win.show()
     sys.exit(app.exec())
-
+0
 main()
