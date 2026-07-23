284a29e40d19b3f1f9243bfbae639348220ddac2
AhmetBedir <ahmet.bedir767@gmail.com>
2026-07-23 18:57:49 +0300

    Otomatik güncelleme işlemi yapıldı.

--- p.py
+++ p.py
@@ -1,1 +1,10 @@
-import PyQt6
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+def main():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.show()
+    sys.exit(app.exec())
+
+main()
